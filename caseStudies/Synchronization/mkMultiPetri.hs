accefccea6835094dbb2d1e15ef67ebb7515d3a2
Wong Meng Weng <mengwong@legalese.com>
2020-10-31 20:10:49 +0800

    propose representations of Synchronization problem in L4

--- caseStudies/Synchronization/mkMultiPetri.hs
+++ caseStudies/Synchronization/mkMultiPetri.hs
@@ -1,8 +1,8 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-16.20 script --package split 
-{-# LANGUAGE OverloadedStrings #-}
--- convert a single untimed-petri dot file to success frames of an animation
+{-# LANGUAGE Haskell2010 #-}
 -- stack ./mkMultiPetri.hs < untimed-petri.dot
+-- convert a single untimed-petri dot file to frames of an animation
 -- "you can write FORTRAN^H^H^H^H^H^H^HPerl in any language"
 
 import Control.Monad (forM_)
@@ -16,17 +16,17 @@ main = do
         , ("anim3", "a_form a_fee > b_review > c_in pa > pa c_out3 > pa a_form") ] (
     \(outfile, tokenSpecs) -> do
       forM_ (zip [1..] (splitOn " > " tokenSpecs)) (
-        \(n,actives) ->
-          writeFile (outfile++"-"++(show n)++".dot") $ unlines $
+        \(frameN,actives) ->
+          writeFile (outfile++"-"++(show frameN)++".dot") $ unlines $
           (flip map) inlines (
           \inline -> let ws  = words  inline
                          len = length inline
-                         insert str = take (len - 3) inline ++ str ++ drop (len -3 ) inline
+                         insert str = take (len - 3) inline ++ str ++ drop (len - 3) inline
                      in if        "\\n\\n\"];" == (drop (len - 7) inline)
                         then if   length ws > 0 && head ws `elem` (words actives)
                              then insert "&bull;"
                              else insert "\\n"
-                        else inline
+                        else id inline
 
           )
         )

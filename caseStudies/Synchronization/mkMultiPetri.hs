dda8368b86ad0dc1c551b930dfb0459ba9dc87e4
Wong Meng Weng <mengwong@legalese.com>
2020-10-31 18:04:32 +0800

    working animation of petri net

--- caseStudies/Synchronization/mkMultiPetri.hs
+++ caseStudies/Synchronization/mkMultiPetri.hs
@@ -2,7 +2,7 @@
 -- stack --resolver lts-16.20 script --package split 
 {-# LANGUAGE OverloadedStrings #-}
 -- convert a single untimed-petri dot file to success frames of an animation
--- mkPetriAnim < untimed-petri.dot
+-- stack ./mkMultiPetri.hs < untimed-petri.dot
 -- "you can write FORTRAN^H^H^H^H^H^H^HPerl in any language"
 
 import Control.Monad (forM_)
@@ -11,9 +11,9 @@ import System.Environment
 
 main = do
   inlines <- lines <$> getContents
-  forM_ [ ("anim1", "a_form a_fee > b_review > c_in pa > c_out1")
-        , ("anim2", "a_form a_fee > b_review > c_in pa > c_out2")
-        , ("anim3", "a_form a_fee > b_review > c_in pa > c_out3 > a_form") ] (
+  forM_ [ ("anim1", "a_form a_fee > b_review > c_in pa > pa c_out1")
+        , ("anim2", "a_form a_fee > b_review > c_in pa > pa c_out2")
+        , ("anim3", "a_form a_fee > b_review > c_in pa > pa c_out3 > pa a_form") ] (
     \(outfile, tokenSpecs) -> do
       forM_ (zip [1..] (splitOn " > " tokenSpecs)) (
         \(n,actives) ->
@@ -21,9 +21,13 @@ main = do
           (flip map) inlines (
           \inline -> let ws  = words  inline
                          len = length inline
-                     in if   length ws > 0 && head ws `elem` (words actives) && "\\n\\n\"];" == (drop (len - 7) inline)
-                        then take (len - 3) inline ++ "&bull;" ++ drop (len -3) inline
+                         insert str = take (len - 3) inline ++ str ++ drop (len -3 ) inline
+                     in if        "\\n\\n\"];" == (drop (len - 7) inline)
+                        then if   length ws > 0 && head ws `elem` (words actives)
+                             then insert "&bull;"
+                             else insert "\\n"
                         else inline
+
           )
         )
     )

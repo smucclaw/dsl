286105b6c3948edfd4446fa417d41b91cec442c7
Inari Listenmaa <inari.listenmaa@gmail.com>
2020-11-04 20:47:25 +0100

    Test case for bike

--- NLG/test.sh
+++ NLG/test.sh
@@ -6,6 +6,8 @@ gf --run < test/negation.gfs | diff -u - test/negation.GOLD
 
 gf --run < test/safe.gfs | diff -u - test/safe.GOLD
 
+gf --run < test/bike.gfs | diff -u - test/bike.GOLD
+
 echo "If this is the first line you see, it means success!"
 
 if [ $# -eq 0 ]
@@ -15,14 +17,17 @@ else
     echo "Short pieces"
     gf --run < test/basic.gfs
 
-    echo ""
     echo "------"
     echo ""
     echo "Negations"
     gf --run < test/negation.gfs
-    echo ""
     echo "------"
     echo ""
     echo "SAFE"
     gf --run < test/safe.gfs
+    echo "------"
+    echo ""
+    echo "Bike"
+    gf --run < test/bike.gfs
+
 fi

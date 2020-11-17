4faa88f5d69676092a28a1d602b377766fb5b365
Andreas Källberg <anka.213@gmail.com>
2020-11-17 11:02:27 +0100

    Add a basic test

--- bnfc/test/Spec.hs
+++ bnfc/test/Spec.hs
@@ -1,2 +1,23 @@
+
+import System.Process(callProcess)
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    -- _ <- runL4 ["l4/test.l4"] ""
+    -- pure ()
+    runL4 ["l4/test.l4"]
+    -- runL4 ["tmp/foo.l4"]
+
+-- runL4 :: [String] -> String -> IO String
+-- runL4 = readProcess "l4"
+runL4 :: [String] -> IO ()
+runL4 = callProcess "l4"
+
+-- stack exec l4 l4/test.l4 > out/test.out 2> out/test.err || stack run showbug l4/test.l4 out/test.out out/test.err
+
+
+-- TODO:
+-- * Run showbug on errors (As library or as executable)
+-- * Run L4 as a library (but maybe also keep some integration test)
+-- * Use a test framework

891ff66dc3cbc4f054dc309141ea28f8b04fb2fa
Inari Listenmaa <inari.listenmaa@gmail.com>
2020-11-25 13:54:45 +0100

    Merge UnifyExpr and ObjMethod
    subchange: remove empty Args rule, make explicit constructors with and
    without Args.

--- bnfc/src-l4/L4.hs
+++ bnfc/src-l4/L4.hs
@@ -17,8 +17,8 @@ data BoolGroup a = AndGroup [a]
 type MyRuleName = String
 
 parseRuleGroup :: Goto -> BoolGroup MyRuleName
-parseRuleGroup (RBreach)                   = IdGroup ("BREACH")
-parseRuleGroup (RFulfilled)                = IdGroup ("FULFILLED")
+parseRuleGroup RBreach                   = IdGroup ("BREACH")
+parseRuleGroup RFulfilled                = IdGroup ("FULFILLED")
 parseRuleGroup (RGotoOne ruledef)          = IdGroup (showRuleDef ruledef)
 parseRuleGroup (RGotoOne ruledef)          = IdGroup (showRuleDef ruledef)
 parseRuleGroup (RGotoLst (ListComma xs))   = AndGroup (exp2rn <$> xs)
@@ -45,7 +45,7 @@ ruleExits r@(Rule rdef rname asof metalimb rulebody) =
     RuleDeem    gu      dls    whw -> whwHenceLest Nothing whw
     RModal      gu ml          whw -> whwHenceLest (Just $ modalDeontic ml) whw
     RMatch mvs                     -> Right $ Solo $ AndGroup $ do
-      (MatchVars24 innerRule) <- mvs
+      (MatchRule innerRule) <- mvs
       return $ showRuleName innerRule
 
 modalDeontic :: ModalLimb -> DeonticExpr
@@ -61,27 +61,27 @@ henceHL       Nothing  _                                  = Right $ NoExit
 henceHL (Just DEMay  ) DNoHence                           = Right $ NoExit
 henceHL (Just DEMust ) DNoHence                           = Right $ Choice (IdGroup "BREACH") (IdGroup "FULFILLED")
 henceHL (Just DEShant) DNoHence                           = Left  $ "SHANT without HENCE or LEST"
-henceHL (Just DEMay  ) (DHeLe  hgoto ha hos lgoto la los) = Left  $ "MAY doesn't go with LEST"
-henceHL (Just DEMust ) (DHeLe  hgoto ha hos lgoto la los) = Right $ Choice (parseRuleGroup lgoto) (parseRuleGroup hgoto) -- TODO: allow multiple HENCE and LEST outputs in the DSL syntax
-henceHL (Just DEShant) (DHeLe  hgoto ha hos lgoto la los) = Right $ Choice (parseRuleGroup lgoto) (parseRuleGroup hgoto)
-henceHL (Just DEMay  ) (DHence RFulfilled      _ _)       = Right $ Solo   (IdGroup "FULFILLED") -- collapsing both left and right; we may want to uncollapse in future.
-henceHL (Just DEMust ) (DHence RFulfilled      _ _)       = Right $ Choice (IdGroup "BREACH") (IdGroup "FULFILLED")
-henceHL (Just DEShant) (DHence RFulfilled      _ _)       = Right $ Choice (IdGroup "BREACH") (IdGroup "FULFILLED")
-henceHL (Just DEMay  ) (DHence (RBreach)       _ _)       = Left  $   "MAY shouldn't cause BREACH; L4 probably should desugar"
-henceHL (Just DEMust ) (DHence (RBreach)       _ _)       = Left  $  "MUST shouldn't cause BREACH; L4 probably should desugar"
-henceHL (Just DEShant) (DHence (RBreach)       _ _)       = Left  $ "SHANT shouldn't cause BREACH; L4 probablyh should desugar"
-henceHL (Just DEMay  ) (DHence hgoto _ _)                 = Right $ Choice (IdGroup "FULFILLED") (parseRuleGroup hgoto) -- unusually, if a MAY is not exercised, we go to FULFILLED
-henceHL (Just DEMust ) (DHence hgoto _ _)                 = Right $ Choice (IdGroup "BREACH")    (parseRuleGroup hgoto)
-henceHL (Just DEShant) (DHence hgoto _ _)                 = Right $ Choice (IdGroup "BREACH")    (parseRuleGroup hgoto)
-henceHL (Just DEMay  ) (DLest  (RBreach)       _ _)       = Left  $ "LEST BREACH is assumed; but where's the HENCE? Or does some other state pick up from here?"
-henceHL (Just DEMust ) (DLest  (RBreach)       _ _)       = Left  $ "LEST BREACH is assumed; but where's the HENCE? Or does some other state pick up from here?"
-henceHL (Just DEShant) (DLest  (RBreach)       _ _)       = Left  $ "LEST BREACH is assumed; but where's the HENCE? Or does some other state pick up from here?"
-henceHL (Just DEMay  ) (DLest  (RFulfilled)    _ _)       = Left  $ "LEST FULFILLED makes no sense; where's the HENCE?"
-henceHL (Just DEMust ) (DLest  (RFulfilled)    _ _)       = Left  $ "LEST FULFILLED makes no sense; where's the HENCE?"
-henceHL (Just DEShant) (DLest  (RFulfilled)    _ _)       = Left  $ "LEST FULFILLED makes no sense; where's the HENCE?"
-henceHL (Just DEMay  ) (DLest  lgoto _ _)       = Left  $ "LEST without HENCE -- " ++ (show $ parseRuleGroup lgoto)
-henceHL (Just DEMust ) (DLest  lgoto _ _)       = Left  $ "LEST without HENCE -- " ++ (show $ parseRuleGroup lgoto)
-henceHL (Just DEShant) (DLest  lgoto _ _)       = Left  $ "LEST without HENCE -- " ++ (show $ parseRuleGroup lgoto)
+henceHL (Just DEMay  ) (DHeLe  hgoto hos lgoto los) = Left  $ "MAY doesn't go with LEST"
+henceHL (Just DEMust ) (DHeLe  hgoto hos lgoto los) = Right $ Choice (parseRuleGroup lgoto) (parseRuleGroup hgoto) -- TODO: allow multiple HENCE and LEST outputs in the DSL syntax
+henceHL (Just DEShant) (DHeLe  hgoto hos lgoto los) = Right $ Choice (parseRuleGroup lgoto) (parseRuleGroup hgoto)
+henceHL (Just DEMay  ) (DHence RFulfilled      _)       = Right $ Solo   (IdGroup "FULFILLED") -- collapsing both left and right; we may want to uncollapse in future.
+henceHL (Just DEMust ) (DHence RFulfilled      _)       = Right $ Choice (IdGroup "BREACH") (IdGroup "FULFILLED")
+henceHL (Just DEShant) (DHence RFulfilled      _)       = Right $ Choice (IdGroup "BREACH") (IdGroup "FULFILLED")
+henceHL (Just DEMay  ) (DHence RBreach      _)       = Left  $   "MAY shouldn't cause BREACH; L4 probably should desugar"
+henceHL (Just DEMust ) (DHence RBreach      _)       = Left  $  "MUST shouldn't cause BREACH; L4 probably should desugar"
+henceHL (Just DEShant) (DHence RBreach      _)       = Left  $ "SHANT shouldn't cause BREACH; L4 probablyh should desugar"
+henceHL (Just DEMay  ) (DHence hgoto _)                 = Right $ Choice (IdGroup "FULFILLED") (parseRuleGroup hgoto) -- unusually, if a MAY is not exercised, we go to FULFILLED
+henceHL (Just DEMust ) (DHence hgoto _)                 = Right $ Choice (IdGroup "BREACH")    (parseRuleGroup hgoto)
+henceHL (Just DEShant) (DHence hgoto _)                 = Right $ Choice (IdGroup "BREACH")    (parseRuleGroup hgoto)
+henceHL (Just DEMay  ) (DLest  RBreach      _)       = Left  $ "LEST BREACH is assumed; but where's the HENCE? Or does some other state pick up from here?"
+henceHL (Just DEMust ) (DLest  RBreach      _)       = Left  $ "LEST BREACH is assumed; but where's the HENCE? Or does some other state pick up from here?"
+henceHL (Just DEShant) (DLest  RBreach      _)       = Left  $ "LEST BREACH is assumed; but where's the HENCE? Or does some other state pick up from here?"
+henceHL (Just DEMay  ) (DLest  RFulfilled   _)       = Left  $ "LEST FULFILLED makes no sense; where's the HENCE?"
+henceHL (Just DEMust ) (DLest  RFulfilled   _)       = Left  $ "LEST FULFILLED makes no sense; where's the HENCE?"
+henceHL (Just DEShant) (DLest  RFulfilled   _)       = Left  $ "LEST FULFILLED makes no sense; where's the HENCE?"
+henceHL (Just DEMay  ) (DLest  lgoto _)       = Left  $ "LEST without HENCE -- " ++ (show $ parseRuleGroup lgoto)
+henceHL (Just DEMust ) (DLest  lgoto _)       = Left  $ "LEST without HENCE -- " ++ (show $ parseRuleGroup lgoto)
+henceHL (Just DEShant) (DLest  lgoto _)       = Left  $ "LEST without HENCE -- " ++ (show $ parseRuleGroup lgoto)
 
 
 --------------------------------------------------------------------------------
@@ -162,13 +162,15 @@ rewriteModal gu mL whw = pure (RModal gu mL whw, Nothing)
 -- BASICS
 --------------------------------------------------------------------------------
 
+pattern MatchRule r = MatchVars23 r
+
 -- retrieve all rules from a parsed module
 getRules :: Tops -> [Rule]
 getRules (Toplevel tops) = fakeRules ++ do
   (ToplevelsRule r@(Rule rdef rname asof metalimb rulebody)) <- tops
   case rulebody of
     RMatch mvs              -> do
-      (MatchVars24 innerRule) <- mvs
+      (MatchRule innerRule) <- mvs
       pure innerRule
     otherwise               -> pure r
 
@@ -178,8 +180,8 @@ fakeRules = mkRule <$> ["FULFILLED", "BREACH"]
 mkRule rulename = Rule (RID $ mkDots rulename) (RName OptLangStrings1) AsofNull Meta0 RBNoop
 
 exp2rn :: Exp -> MyRuleName
-exp2rn (ObjME (ObjMethod1 uelist args ols)) = uelist2rn uelist
-exp2rn (UnifyE (UnifyExpr1 ueoaes)) = intercalate "." (showUEOAE <$> ueoaes)
+exp2rn (ObjME (OMArgs uelist args ols)) = uelist2rn uelist
+exp2rn (ObjME (OMNoAargs ueoaes ols)) = intercalate "." (showUEOAE <$> ueoaes)
 exp2rn other = error ("don't know how to convert expression to rulename: " ++ show other)
 
 uelist2rn :: [UnifyElem] -> MyRuleName

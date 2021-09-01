609f96caa4b71dabab17dfade7e660add964a373
Wong Meng Weng <mengwong@legalese.com>
2021-09-02 02:18:18 +0800

    start work on relevance painter

---
+++ lib/haskell/anyall/src/AnyAll/Relevance.hs
@@ -0,0 +1,11 @@
+module AnyAll.Relevance where
+
+import AnyAll.Types
+
+
+
+-- given a tree
+-- and a marking indicating which values are known,
+-- return a marking indicating which nodes in the tree are still of interest.
+
+  

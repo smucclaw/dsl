4cace3885185490c58bb9d9a98a7be6880b30516
Wong Meng Weng <mwwong@smu.edu.sg>
2021-11-23 14:14:52 +0800

    rename BabyL4 to CoreL4

--- mengwong/mp/src/LS/XPile/CoreL4.hs
+++ mengwong/mp/src/LS/XPile/CoreL4.hs
@@ -1,24 +1,24 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NamedFieldPuns #-}
-module LS.XPile.BabyL4 where
+module LS.XPile.CoreL4 where
 
-import L4.Syntax as BabyL4
+import L4.Syntax as CoreL4
 
 import LS.Types as SFL4
 import L4.Annotation
 import Data.Text.Lazy (unpack)
 
--- output to Baby L4 for further transformation
+-- output to Core L4 for further transformation
 
 sfl4Dummy :: SRng
 sfl4Dummy = DummySRng "From spreadsheet"
 
-sfl4ToBabyl4 :: [SFL4.Rule] -> BabyL4.Program SRng
-sfl4ToBabyl4 rus
-  = Program {annotOfProgram = sfl4Dummy, elementsOfProgram = map sfl4ToBabyl4Rule rus}
+sfl4ToCorel4 :: [SFL4.Rule] -> BabyL4.Program SRng
+sfl4ToCorel4 rus
+  = Program {annotOfProgram = sfl4Dummy, elementsOfProgram = map sfl4ToCorel4Rule rus}
 
-sfl4ToBabyl4Rule :: SFL4.Rule -> TopLevelElement SRng
-sfl4ToBabyl4Rule Regulative
+sfl4ToCorel4Rule :: SFL4.Rule -> TopLevelElement SRng
+sfl4ToCorel4Rule Regulative
             { subj     -- every person
             , keyword  -- every / party / all
             , who      -- who walks and (eats or drinks)
@@ -36,7 +36,7 @@ sfl4ToBabyl4Rule Regulative
             , having   -- HAVING sung...
             , orig
             } = undefined
-sfl4ToBabyl4Rule Constitutive
+sfl4ToCorel4Rule Constitutive
             { name     -- the thing we are defining
             , keyword  -- Means, Includes, Is, Deem
             , letbind  -- might be just a bunch of words to be parsed downstream
@@ -47,7 +47,7 @@ sfl4ToBabyl4Rule Constitutive
             , srcref
             , orig
             } = undefined
-sfl4ToBabyl4Rule TypeDecl
+sfl4ToCorel4Rule TypeDecl
             { name     --      DEFINE Sign
             , super    --                 
             , has      -- HAS foo :: List Hand \n bar :: Optional Restaurant
@@ -56,12 +56,12 @@ sfl4ToBabyl4Rule TypeDecl
             , lsource
             , srcref
             } = ClassDeclTLE (ClassDecl {annotOfClassDecl = sfl4Dummy, nameOfClassDecl =  ClsNm $ unpack name, defOfClassDecl = ClassDef [] []})
-sfl4ToBabyl4Rule DefNameAlias -- inline alias, like     some thing AKA Thing
+sfl4ToCorel4Rule DefNameAlias -- inline alias, like     some thing AKA Thing
             { name   -- "Thing"
             , detail -- "some thing"
             , nlhint -- "lang=en number=singular"
             , srcref
             } = undefined
-sfl4ToBabyl4Rule (RuleAlias t) = undefined -- internal softlink to a constitutive rule label = _
-sfl4ToBabyl4Rule RegFulfilled = undefined -- trivial top = _
-sfl4ToBabyl4Rule RegBreach    = undefined -- trivial bottom
+sfl4ToCorel4Rule (RuleAlias t) = undefined -- internal softlink to a constitutive rule label = _
+sfl4ToCorel4Rule RegFulfilled = undefined -- trivial top = _
+sfl4ToCorel4Rule RegBreach    = undefined -- trivial bottom

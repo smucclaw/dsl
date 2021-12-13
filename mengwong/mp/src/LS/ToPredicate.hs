4991ddc712bac69d8731fd1c65f1241d3e7c8a17
Inari Listenmaa <inari.listenmaa@gmail.com>
2021-12-13 13:26:09 +0800

    Support binary predicates

--- mengwong/mp/src/LS/ToPredicate.hs
+++ mengwong/mp/src/LS/ToPredicate.hs
@@ -10,7 +10,7 @@ import Data.List.Split (splitOn)
 import Data.Char (toUpper)
 
 
-data Predicate = Not Predicate | Unary String
+data Predicate = Not Predicate | Unary String | Binary String
   deriving (Show, Eq)
 
 convertToPredicate :: Expr -> Predicate
@@ -26,10 +26,25 @@ headName = removeType . showCId
 data SomeTree = forall b. SomeTree (Tree b)
 
 findHead :: Gf (Tree a) => Tree a -> Predicate
-findHead (Groot_only x) = findHead x
 findHead (GrootN_ x) = Unary $ headNP x
+findHead (GrootV_ x) = Unary $ headVP x
+findHead (Groot_only rt) = findHead rt
 findHead (Groot_cop_advmod rt _cp Gnot_advmod) = Not $ findHead rt
+findHead (Groot_mark_nsubj rt _ _) = findHead rt
+findHead (Groot_nsubj rt _) = findHead rt
+--findHead (Groot_xcomp rt xc) = mashTogether
+findHead (Groot_xcomp_ccomp (GrootV_ vp) xc (Gccomp_ uds)) =
+    Unary (headVP vp `combineName` headXC xc)
+     `combinePredicate` findHead uds
 findHead x = error $ "don't know how to find the head from " ++ showExpr [] (gf x)
+
+headXC :: Gxcomp -> String
+headXC (GxcompA_ a) = headAP a
+headXC (GxcompAdv_ adv) = error "not implemented: headXC"
+
+headVP :: GVP -> String
+headVP (GUseV v) | Just (v', []) <- unApp (gf v) = headName v'
+headVP _ = error "not implemented"
 
 headNP :: GNP -> String
 headNP (GMassNP cn) = headCN cn
@@ -48,31 +63,23 @@ handleAnaphora = error "not implemented"
 
 headCN :: GCN -> String
 headCN (GUseN n) | Just (n', []) <- unApp (gf n) = headName n' -- organization
-headCN (GAdjCN ap cn) = mashTogether (headAP ap) (headCN cn) -- publicAgency_N
+headCN (GAdjCN ap cn) = combineName (headAP ap) (headCN cn) -- publicAgency_N
 headCN _ = error "not implemented"
 
 headAP :: GAP -> String
 headAP (GPositA a) | Just (a', []) <- unApp (gf a) = headName a'
 headAP _ = error "not implemented"
 
+infixr 2 `combineName`
+infixr 2 `combinePredicate`
 
-mashTogether :: String -> String -> String
-mashTogether a n = a ++ capitalize n
+combineName :: String -> String -> String
+combineName a n = a ++ capitalize n
+
+combinePredicate :: Predicate -> Predicate -> Predicate
+combinePredicate (Unary p1) (Unary p2) = Binary $ p1 `combineName` p2
 
 capitalize :: String  -> String
 capitalize (a:as) = toUpper a : as
-capitalize [] = error "empty string"
+capitalize [] = []
 
-{-}
-NP -> N
-VP -> V
-AP -> A
-
-rootA_ exp -> extract xxx_A from exp
-rootN_ exp -> extract xxx_N from exp
-
-rootV_ exp -> extract xxx_V from exp
-rootAdv_ exp -> try: extract xxx_Adv from exp
-                try: extract PrepNP xxx_Prep yyy_NP from exp
-
-    -}
--- mengwong/mp/test/TestNLG.hs
+++ mengwong/mp/test/TestNLG.hs
@@ -27,3 +27,17 @@ nlgTests = do
       it "Should convert root_cop_advmod to a negated unary predicate" $ do
         convertToPredicate notPub `shouldBe` Not (Unary "publicAgency")
 
+      -- "a data breach occurs"
+      let Just occurs = readExpr "root_mark_nsubj (rootV_ (UseV occur_V)) (mark_ that_Subj) (nsubj_ (DetCN (DetQuant IndefArt NumSg) (UseN (CompoundN data_N breach_N))))"
+      it "Should convert root_mark_nsubj to a unary predicate" $ do
+        convertToPredicate occurs `shouldBe` Unary "occur"
+
+     -- "become aware"
+    --   let Just aware = readExpr "root_xcomp (rootV_ (UseV become_V)) (xcompA_ (PositA aware_A))"
+    --   it "Should convert root_xcomp to a unary predicate" $ do
+    --     convertToPredicate aware `shouldBe` Unary "becomeAware"
+
+      -- "become aware that a data breach occurs"
+      let Just becomingAware = readExpr "root_xcomp_ccomp (rootV_ (UseV become_V)) (xcompA_ (PositA aware_A)) (ccomp_ (root_mark_nsubj (rootV_ (UseV occur_V)) (mark_ that_Subj) (nsubj_ (DetCN (DetQuant IndefArt NumSg) (UseN (CompoundN data_N breach_N))))))"
+      it "Should convert root_*_ccomp into a binary predicate" $ do
+        convertToPredicate becomingAware `shouldBe` Binary "becomeAwareOccur"

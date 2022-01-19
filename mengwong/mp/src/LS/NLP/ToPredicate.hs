dbb7168d3138207f342e580542841ea47dd2de78
Inari Listenmaa <inari.listenmaa@gmail.com>
2022-01-19 18:21:43 +0800

    Documentation + cleanup

--- mengwong/mp/src/LS/NLP/ToPredicate.hs
+++ mengwong/mp/src/LS/NLP/ToPredicate.hs
@@ -68,47 +68,59 @@ mkPredicate (Groot_ccomp (GrootV_ vp) (Gccomp_ uds)) =
 mkPredicate (Groot_nsubj_ccomp rt subj cc) = undefined
 mkPredicate x = error $ "don't know how to find the head from " ++ showExpr [] (gf x)
 
+-- Used when there are ccomp (clausal complement) present in the sentence
+-- We need to find, not just the head, but also the argument of the ccomp
 findHeadAndArg :: Tree a -> (Predicate, String)
-findHeadAndArg uds | [] <- findCcomp uds
+findHeadAndArg uds | [] <- findCcomp uds  -- (org) becomes aware that breach occurs
                    , [root] <- findRoot uds
                    , [sub] <- findNsubj uds = (mkPredicate root, headNP sub)
-findHeadAndArg uds | [cc] <- findCcomp uds
+findHeadAndArg uds | [cc] <- findCcomp uds -- (org) becomes aware that lawyer knows that breach occurs
                    , [knowRoot] <- findRoot uds
                    , [lawyerSubj] <- findNsubj uds
                    , (Unary occur, databreach) <- findHeadAndArg cc
                    , (Unary know, lawyer) <- (mkPredicate knowRoot, headNP lawyerSubj)
                    = (Binary (know `combineName` occur) lawyer, databreach)
-
+-- Need to manually add support for each number of nested ccomps
 findHeadAndArg uds | roots <- findRoot uds, nps <- findNsubj uds = error $ "too many roots:\n "
   ++ intercalate ", " (map (showExpr [] . gf) roots)
   ++ "\n and nsubjs\n"
   ++ intercalate ", " (map (showExpr [] . gf) nps)
 
-findNsubj :: Tree a -> [GNP]
-findNsubj (Gnsubj_ np) = [np]
-findNsubj c@(Gccomp_ uds) = []
-findNsubj x = composOpMonoid findNsubj x
-
 findCcomp :: Tree a -> [GUDS]
 findCcomp (Gccomp_ uds) = [uds]
 findCcomp x = composOpMonoid findCcomp x
 
+-- Finds a NP labelled with nsubj, but skips clausal complements (ccomp).
+-- So for a sentence like "I know that you know", it will only return [i_NP].
+-- The list is there just to not crash when there are no nsubjs,
+-- we don't expect a sentence to have multiple nsubjs, aside from the excluded ccomp.
+findNsubj :: Tree a -> [GNP]
+findNsubj (Gnsubj_ np) = [np]
+findNsubj (Gccomp_ _uds) = [] -- when finding ccomp, return empty list, don't look further for nsubjs
+findNsubj x = composOpMonoid findNsubj x
+
+-- Finds negation, but skips ccomp(s)
+-- "I don't know that you know" -> returns negation
+-- "I know that you don't know" -> returns empty list
 findNeg :: Tree a -> [Gadvmod]
 findNeg Gnot_advmod = [Gnot_advmod]
 findNeg (Gccomp_ _) = []
 findNeg x = composOpMonoid findNeg x
 
+-- Finds root, but skips ccomp(s)
+-- "I know that you sing" -> returns [know], not sing
 findRoot :: Tree a -> [Groot]
-findRoot rt@(GrootA_ ap) = [rt]
-findRoot rt@(GrootN_ np) = [rt]
-findRoot rt@(GrootV_ vp) = [rt]
-findRoot c@(Gccomp_ uds) = []
+findRoot rt@(GrootA_ _ap) = [rt]
+findRoot rt@(GrootN_ _np) = [rt]
+findRoot rt@(GrootV_ _vp) = [rt]
+findRoot    (Gccomp_ _)   = []
 findRoot x = composOpMonoid findRoot x
 
 
 headXC :: Gxcomp -> String
 headXC (GxcompA_ a) = headAP a
-headXC (GxcompAdv_ adv) = error "not implemented: headXC"
+headXC (GxcompAdv_ adv) = headAdv adv
+headXC (GxcompA_ccomp_ ap cc) = error "not implemented"
 
 headVP :: GVP -> String
 headVP (GUseV v) | Just (v', []) <- unApp (gf v) = headName v'
@@ -141,8 +153,13 @@ headCN (GAdjCN ap cn) =        -- publicAgency
 headCN _ = error "not implemented"
 
 headAP :: GAP -> String
-headAP (GPositA a) | Just (a', []) <- unApp (gf a) = headName a'
+headAP (GPositA (Lexical a)) = a
 headAP _ = error "not implemented"
+
+headAdv :: GAdv -> String
+headAdv (GPrepNP (LexPrep pr) np) = pr `combineName` headNP np
+headAdv (LexAdv adv) = adv
+headAdv _ = error "not implemented"
 
 infixr 2 `combineName`
 infixr 2 `combinePredicate`
@@ -153,6 +170,9 @@ combineName a n = a ++ capitalize n
 combinePredicate :: Predicate -> (Predicate, Arg) -> Predicate
 combinePredicate (Unary p1) (Unary p2, arg) = Binary (p1 `combineName` p2) arg
 combinePredicate (Unary p1) (Binary p2 arg1, arg2) = Ternary (p1 `combineName` p2) arg1 arg2
+--combinePredicate (Unary p1) ((Not pred), arg) = _ -- TODO: put negation in the name only
+combinePredicate _ _ = error "combinePredicate: not implemented"
+
 
 capitalize :: String  -> String
 capitalize (a:as) = toUpper a : as

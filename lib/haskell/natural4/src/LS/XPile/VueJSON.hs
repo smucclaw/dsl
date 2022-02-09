e77e7d17d7d500282ef26df15025e68874780343
Meng Weng Wong <mengwong@gmail.com>
2022-02-09 17:17:44 +0800

    bit of NLG sugar to make the rule grounds into a checklist

--- lib/haskell/natural4/src/LS/XPile/VueJSON.hs
+++ lib/haskell/natural4/src/LS/XPile/VueJSON.hs
@@ -9,10 +9,11 @@ import AnyAll.Types
 import Options.Generic
 import Data.Maybe (maybeToList, catMaybes)
 import Data.List (nub)
+import qualified Data.Text.Lazy as Text
 
 -- https://en.wikipedia.org/wiki/Ground_expression
 groundrules :: RunConfig -> [Rule] -> [MultiTerm]
-groundrules rc rs = nub $ concatMap (rulegrounds rc globalrules) rs
+groundrules rc rs = groundToChecklist <$> (nub $ concatMap (rulegrounds rc globalrules) rs)
   where
     globalrules :: [Rule]
     globalrules = [ r
@@ -20,7 +21,7 @@ groundrules rc rs = nub $ concatMap (rulegrounds rc globalrules) rs
 
 rulegrounds :: RunConfig -> [Rule] -> Rule -> [MultiTerm]
 rulegrounds rc globalrules r@Regulative{..} =
-  let whoGrounds  = (bsp2text subj :) <$> bsr2grounds who
+  let whoGrounds  = ("the " <> bsp2text subj :) <$> bsr2grounds who
       condGrounds =                       bsr2grounds cond
   in concat [whoGrounds, condGrounds]
   where bsr2grounds = concat . maybeToList . fmap (aaLeavesFilter (ignoreTypicalRP rc globalrules r))
@@ -39,3 +40,14 @@ hasDefaultValue r rp = rpHead rp `elem` (rpHead <$> defaults r)
 
 defaultInGlobals :: [Rule] -> RelationalPredicate -> Bool
 defaultInGlobals rs rp = any (`hasDefaultValue` rp) rs
+
+
+-- meng's crude natural language conversion
+-- this is to be read as an "external requirement interface"
+-- the implementation is totally up to the NLG team who can make use of more sophisticated code
+-- to achieve the same goals.
+-- As a starting point, we begin with hard-coded conversion functions.
+
+groundToChecklist :: MultiTerm -> [Text.Text]
+groundToChecklist mts =
+  pure $ Text.unwords mts

1caf38f8e22bf69950fc4e183b81ca1ac858a771
Inari Listenmaa <inari.listenmaa@gmail.com>
2022-02-18 17:13:34 +0800

    script to update UDExt.hs

---
+++ lib/haskell/natural4/grammars/updateHS.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gf -make -f haskell --haskell=gadt --haskell=lexical --lexical=N,V,A,N2,N3,V2,A2,VA,V2V,VV,V3,VS,V2A,V2S,V2Q,Adv,AdV,AdA,AdN,ACard,CAdv,Conj,Interj,PN,Prep,Pron,Quant,Det,Card,Text,Predet,Subj UDExtEng.gf
+sed -i "" 's/module UDExt where/module LS.NLP.UDExt where/ ; s/instance Show .*//' UDExt.hs
+mv UDExt.hs ../src/LS/NLP/

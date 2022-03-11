160f0427ed668e25eeb12ada6c636324669cf044
Meowyam <maryam@chilon.net>
2022-03-11 11:24:18 +0800

    changed updateHS for unix

--- lib/haskell/natural4/grammars/updateHS.sh
+++ lib/haskell/natural4/grammars/updateHS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gf -make -f haskell --haskell=gadt --haskell=lexical --lexical=N,V,A,N2,N3,V2,A2,VA,V2V,VV,V3,VS,V2A,V2S,V2Q,Adv,AdV,AdA,AdN,ACard,CAdv,Conj,Interj,PN,Prep,Pron,Quant,Det,Card,Text,Predet,Subj UDExtEng.gf
-sed -i "" 's/module UDExt where/{-# OPTIONS_GHC -Wno-all #-}\nmodule LS.NLP.UDExt where/ ; s/instance Show .*//' UDExt.hs
+sed 's/module UDExt where/{-# OPTIONS_GHC -Wno-all #-}\nmodule LS.NLP.UDExt where/ ; s/instance Show .*//' UDExt.hs > /tmp/whatever.hs
 # 
-mv UDExt.hs ../src/LS/NLP/
+mv /tmp/whatever.hs ../src/LS/NLP/UDExt.hs

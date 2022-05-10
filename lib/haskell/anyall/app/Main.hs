cdd1480c1401436edcb048ede4449429b1627f3f
Wong Meng Weng <mwwong@smu.edu.sg>
2022-05-10 16:34:49 +0800

    started implementation of new visualization approach

--- lib/haskell/anyall/app/Main.hs
+++ lib/haskell/anyall/app/Main.hs
@@ -46,7 +46,7 @@ main = do
   when (only opts == "tree") $
     ppQTree (andOrTree myright) (getDefault <$> (getMarking $ marking myright))
   when (only opts == "svg") $
-    print $ q2svg defaultConfig $ hardnormal (marking myright) (andOrTree myright) 
+    print $ makeSvg $ q2svg' defaultAAVConfig $ hardnormal (marking myright) (andOrTree myright) 
   where
     -- | every item in the and/or tree should be marked with some explicit value, whether user-provided or computed
     

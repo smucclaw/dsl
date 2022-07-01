2b84ea2db96755d1240b90edb091d384e225b7b0
Wong Meng Weng <mwwong@smu.edu.sg>
2022-07-01 17:05:47 +0800

    the json dumped by anyall is not quite the same as the json expects, sigh

--- lib/haskell/natural4/app/Main.hs
+++ lib/haskell/natural4/app/Main.hs
@@ -71,6 +71,7 @@ main = do
           mywritefile False dname (fname<>"-full")   ext (show svgfull)
           mywritefile False dname (fname<>"-anyall") "hs" (TL.unpack $ pShowNoColor hsAnyAllTree)
           mywritefile False dname (fname<>"-qtree")  "hs" (TL.unpack $ pShowNoColor hsQtree)
+          mywritefile False dname (fname<>"-qjson") "json" (toString $ encodePretty hsQtree)
           let fnamext = fname <> "." <> ext
               displayTxt = Text.unpack $ Text.unwords n
           appendFile (dname <> "/index.html") ("<li> " <> "<a href=\"" <> fnamext <> "\">" <> displayTxt <> "</a></li>\n")

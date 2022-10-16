7e9a815ae58c09213f51ac3477bdd777d6313402
Meng Weng Wong <mengwong@gmail.com>
2022-10-16 23:43:20 +0800

    output more SVG

--- lib/haskell/natural4/src/LS/XPile/SVG.hs
+++ lib/haskell/natural4/src/LS/XPile/SVG.hs
@@ -16,6 +16,7 @@ import AnyAll as AA
 import qualified Data.Map as Map
 import qualified Data.Text as T
 import Data.Maybe (maybeToList)
+import Debug.Trace (trace)
 
 -- | extract the tree-structured rules from Interpreter
 -- for each rule, print as svg according to options we were given
@@ -24,13 +25,20 @@ asAAsvg :: AAVConfig -> Interpreted -> [Rule] -> Map.Map RuleName (SVGElement, S
 asAAsvg aavc l4i rs =
   let rs' = stitchRules l4i rs -- connect up the rules internally, expand HENCE and LEST rulealias links, expand defined terms
       rs2 = groupedByAOTree l4i rs
-  in Map.fromList [ (rn, (svgtiny, svgfull, aaT, qtree))
+  in Map.fromList [ (rn ++ [T.pack (show rn_n)], (svgtiny, svgfull, aaT, qtree))
                   | (_,rulegroup) <- rs2
                   , not $ null rulegroup
                   , let r = Prelude.head rulegroup
-                  , let rn      = ruleLabelName r
-                  , aaT <- fmap rp2text <$> (expandBSR l4i 1 <$> getBSR r)
-                  , let qtree   = hardnormal (cgetMark aavc) aaT
+                        rn      = ruleLabelName r
+                        ebsr = expandBSR l4i 1 <$>
+                               trace ("asAAsvg getBSR = " ++ show (getBSR r))
+                               (getBSR r)
+                        totext = fmap rp2text <$> trace ("asAAsvg expandBSR = " ++ show ebsr) ebsr
+                  , (rn_n, aaT) <- zip [1..] $ trace ("asAAsvg aaT <- totext = " ++ show totext)
+                                   totext
+                  , let qtree   = hardnormal (cgetMark aavc) $
+                                  trace ("asAAsvg aaT = " ++ show aaT)
+                                  aaT
                         svgtiny = makeSvg $ q2svg' aavc { cscale = Tiny } qtree
                         svgfull = makeSvg $ q2svg' aavc { cscale = Full } qtree
                   ]

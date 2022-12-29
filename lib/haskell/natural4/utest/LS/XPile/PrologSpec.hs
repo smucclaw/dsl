ed368e4a0d8a9efb9ec615dfc891a01297cb97c5
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2022-12-29 10:18:10 +0800

    Add bsp2structDT

---
+++ lib/haskell/natural4/utest/LS/XPile/PrologSpec.hs
@@ -0,0 +1,93 @@
+{-# LANGUAGE OverloadedStrings #-}
+module LS.XPile.PrologSpec where
+
+import Test.Hspec
+import LS.Types
+import AnyAll
+import LS.XPile.Prolog
+import Data.List.NonEmpty
+import AnyAll.BoolStructTree
+
+spec :: Spec
+spec = do
+  describe "bsp2struct" $ do
+    it "Leaf" $ do
+      let
+        typeSig = Just (SimpleType TOne "Fruit")
+        typedWords = "apple" :| ["orange", "banana"]
+        rp = (typedWords, typeSig) :| []
+        l = mkLeaf rp
+      bsp2struct l `shouldBe` [vart "apple orange banana"]
+
+    it "Not" $ do
+      let
+        typeSig = Just (SimpleType TOne "Fruit")
+        typedWords = "apple" :| ["orange", "banana"]
+        rp = (typedWords, typeSig) :| []
+        l = mkNot $ mkLeaf rp
+      bsp2struct l `shouldBe` vart "neg" : [vart "apple orange banana"]
+
+    it "Any" $ do
+      let
+        fruitType = Just (SimpleType TOne "Fruit")
+        fruitWords = "apple" :| ["orange", "banana"]
+        l1 = (fruitWords, fruitType) :| []
+
+        colorType = Just (SimpleType TOne "Color")
+        l2 = ("red" :| ["orange", "yellow"], colorType) :| []
+    
+        l = mkAny Nothing [mkLeaf l1, mkLeaf l2]
+      bsp2struct l `shouldBe` vart "or" : [vart "apple orange banana", vart "red orange yellow"]
+
+    it "All" $ do
+      let
+        fruitType = Just (SimpleType TOne "Fruit")
+        fruitWords = "apple" :| ["orange", "banana"]
+        l1 = (fruitWords, fruitType) :| []
+
+        colorType = Just (SimpleType TOne "Color")
+        l2 = ("red" :| ["orange", "yellow"], colorType) :| []
+    
+        l = mkAll Nothing [mkLeaf l1, mkLeaf l2]
+      bsp2struct l `shouldBe` [vart "apple orange banana", vart "red orange yellow"]
+
+  describe "bsp2structDT" $ do
+    it "Leaf" $ do
+      let
+        typeSig = Just (SimpleType TOne "Fruit")
+        typedWords = "apple" :| ["orange", "banana"]
+        rp = (typedWords, typeSig) :| []
+        l = mkLeafDT rp
+      bsp2structDT l `shouldBe` [vart "apple orange banana"]
+
+    it "Not" $ do
+      let
+        typeSig = Just (SimpleType TOne "Fruit")
+        typedWords = "apple" :| ["orange", "banana"]
+        rp = (typedWords, typeSig) :| []
+        l = mkNotDT $ mkLeafDT rp
+      bsp2structDT l `shouldBe` vart "neg" : [vart "apple orange banana"]
+
+    it "Any" $ do
+      let
+        fruitType = Just (SimpleType TOne "Fruit")
+        fruitWords = "apple" :| ["orange", "banana"]
+        l1 = (fruitWords, fruitType) :| []
+
+        colorType = Just (SimpleType TOne "Color")
+        l2 = ("red" :| ["orange", "yellow"], colorType) :| []
+    
+        l = mkAnyDT Nothing [mkLeafDT l1, mkLeafDT l2]
+      bsp2structDT l `shouldBe` vart "or" : [vart "apple orange banana", vart "red orange yellow"]
+
+    it "All" $ do
+      let
+        fruitType = Just (SimpleType TOne "Fruit")
+        fruitWords = "apple" :| ["orange", "banana"]
+        l1 = (fruitWords, fruitType) :| []
+
+        colorType = Just (SimpleType TOne "Color")
+        l2 = ("red" :| ["orange", "yellow"], colorType) :| []
+    
+        l = mkAllDT Nothing [mkLeafDT l1, mkLeafDT l2]
+      bsp2structDT l `shouldBe` [vart "apple orange banana", vart "red orange yellow"]

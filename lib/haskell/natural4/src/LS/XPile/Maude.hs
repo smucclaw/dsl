9610864a5d93333c8f6b531e526b169192bf922d
Joe Watt <joewatt95@u.nus.edu>
2023-01-27 13:38:31 +0800

    Relax Traversable -> Foldable and add test case

--- lib/haskell/natural4/src/LS/XPile/Maude.hs
+++ lib/haskell/natural4/src/LS/XPile/Maude.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 {-
   Work-in-progress transpiler to Maude.
@@ -9,50 +11,88 @@
 -}
 module LS.XPile.Maude where
 
+import Data.Text qualified as T
+
 import LS.Types
     ( Deontic(DShant, DMust, DMay),
       TemporalConstraint(TemporalConstraint),
       RegKeywords(RParty) )
 import LS.Rule
-    ( Rule(Regulative, lest, rlabel, rkeyword, deontic, action,
-           temporal, hence) )
+    ( Rule(..) )
 
-import Prettyprinter ( Doc, cat, vcat, viaShow, Pretty(pretty), concatWith, line )
-import Flow ( (|>) )
+import Flow ( (.>) , (|>) )
+import Prettyprinter (Doc, Pretty (pretty), line, viaShow, hsep)
 
 -- This function is still a work in progress.
-rule2pretty :: Rule -> Doc ann
-rule2pretty
+rule2doc :: Rule -> Doc ann
+rule2doc
   Regulative
     { rlabel = Just (_, _, ruleName),
       rkeyword = RParty,
+      subj,
       deontic,
       action,
       temporal = Just tempConstr@(TemporalConstraint _ _ _),
       hence,
       lest
     } =
-      [ ["RULE '", pretty ruleName],
-        [deontic2str deontic, "DO '", viaShow action],
-        [viaShow tempConstr],
-        ["'", viaShow hence],
-        ["'", viaShow lest]
-      ]
-      |$> cat |> vcat
+    [ ["RULE", ruleNameQid],
+      ["PARTY", show2Qid subj],
+      [deontic2str deontic, "DO", show2Qid action],
+      [viaShow tempConstr],
+      [show2Qid hence],
+      [show2Qid lest]
+    ]
+    |> mapThenCatWith hsep catWith1line
     where
+      catWith1line = catWithNLines 1
       deontic2str DMust = "MUST"
       deontic2str DMay = "MAY"
       deontic2str DShant = "SHANT"
+      makeQid x = "'" <> x
+      show2Qid x = x |> viaShow |> makeQid
+      ruleNameQid = ruleName |> pretty |> makeQid
 
-rules2maude :: Traversable m => m Rule -> String
-rules2maude rules = rules |$> rule2pretty |> vvcat |> show
+rules2doc :: Foldable t => t Rule -> Doc ann
+rules2doc = mapThenCatWith rule2doc catWith2lines
   where
-    vvcat = concatWith f
-    f x y = mconcat [x, line, line, y]
+    catWith2lines = catWithNLines 2
+
+rules2maudeStr :: Foldable t => t Rule -> String
+rules2maudeStr = rules2doc .> show
+
+test :: String
+test = rules2maudeStr [ Regulative {..} ]
+  where
+    rlabel = Just (undefined, undefined, T.pack "rule")
+    rkeyword = RParty
+    subj = undefined
+    deontic = DMust
+    action = undefined
+    temporal = Just (TemporalConstraint undefined undefined undefined)
+    hence = Nothing
+    lest = Nothing
+
+    given = Nothing
+    having = Nothing
+    who = Nothing
+    cond = Nothing
+    lsource = Nothing
+    srcref = Nothing
+    upon = Nothing
+    wwhere = []
+    defaults = []
+    symtab = []
 
 -- Utilities.
 
-infixl 0 |$>
+mapThenCatWith ::
+  (Foldable t1, Monoid b) => (a -> t2) -> (t2 -> b -> b) -> t1 a -> b
+mapThenCatWith mapper combiner = foldr combiner' mempty
+  where
+    combiner' x = combiner (mapper x)
 
-(|$>) :: Functor f => f a -> (a -> b) -> f b
-x |$> f = fmap f x
+catWithNLines :: Int -> Doc ann -> Doc ann -> Doc ann
+catWithNLines n x y = x <> lines' <> y
+  where
+    lines' = line |> repeat |> take n |> mconcat

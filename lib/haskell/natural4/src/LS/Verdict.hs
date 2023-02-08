c929dfbd8afe940276a0b37d9541039e37696cd4
Meng Weng Wong <mengwong@gmail.com>
2023-02-08 13:28:34 +0800

    example of how natural4 can consume Explainable

---
+++ lib/haskell/natural4/src/LS/Verdict.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+
+{-|
+
+The Verdict module provides a runtime evaluation engine to deliver
+answers based on available input.
+
+Typical usage: some application executable obtains input data from the
+end-user, sets up a query, and returns an answer, with accompanying
+explanation.
+
+-}
+
+module LS.Verdict where
+
+import           LS.Interpreter
+import           LS.Rule
+
+import qualified AnyAll                 as AA
+
+import           Explainable
+import           Explainable.MathLang
+
+-- | User input takes the form of a Marking -- see AnyAll for more on what that means.
+type RuntimeInput = AA.TextMarking
+
+-- | What is the output of partial evaluation? a residual ruleset?
+-- Let's start simple with just a true or false.
+type RuntimeOutput =
+  Bool
+  -- [Rule]
+
+-- | Let's compute a verdict, along with explanations.
+l4verdict :: Interpreted
+          -> RuntimeInput
+          -> (RuntimeOutput, [String])
+l4verdict l4i rti =
+  (True, ["Explainability goes here"])

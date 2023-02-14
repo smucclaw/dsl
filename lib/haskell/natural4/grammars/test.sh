b624a8563e1e60e2826b8f6ccbae72e0186e7d22
Inari Listenmaa <inari.listenmaa@gmail.com>
2023-02-14 09:23:28 +0800

    remove old grammars etc.

--- lib/haskell/natural4/grammars/test.sh
+++
@@ -1,6 +0,0 @@
-echo "basic tests"
-cat tests/basic.conllu | gf-ud ud2gf UDApp Eng UDS at lin no-backups | diff -u - tests/basic.conllu.gold
-
-echo ""
-echo "more complex tests"
-cat tests/succeeds.conllu | gf-ud ud2gf UDApp Eng UDS at lin no-backups | diff -u - tests/succeeds.conllu.gold

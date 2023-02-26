7871198d3ce0db384fc7c8f5975d7e7282331bc6
Meng Weng Wong <mengwong@gmail.com>
2023-02-27 00:50:21 +0800

    continue to implement global vs local vars, hasty improvements to ts

--- lib/haskell/natural4/src/LS/XPile/Typescript.hs
+++ lib/haskell/natural4/src/LS/XPile/Typescript.hs
@@ -29,16 +29,17 @@ import qualified Data.Map as Map
 -- import qualified Data.Text as T
 -- import Data.Maybe (catMaybes, fromMaybe, isJust)
 -- import qualified Data.List.NonEmpty as NE
-import Data.List (nub)
-
+import Data.List (nub, intercalate)
+import Data.Maybe (isJust)
+import qualified Data.Tree as DT
 
 asTypescript :: [SFL4R.Rule] -> Doc ann
 asTypescript rs =
   let l4i = l4interpret defaultInterpreterOptions rs
   in
     vvsep [ tsClasses l4i
-         , jsInstances l4i
-         ]
+          , globalDefinitions l4i
+          ]
 
 tsClasses :: Interpreted -> Doc ann
 tsClasses l4i =
@@ -65,21 +66,23 @@ tsClasses l4i =
         , (Just (_ctype, children)) <- [Map.lookup className ch]
         ]
 
--- classes and variables need to be topologically sorted because typescript is picky about that.
-
+-- | all the symbols we know about, including those that show up in GIVEN, DEFINE, and DECIDE.
+-- This is probably not the best way to do it because we're commingling local and global variables here.
+-- Happy to rethink all of this.
+-- Also, not yet done, classes and variables need to be topologically sorted because typescript is picky about that.
 jsInstances :: Interpreted -> Doc ann
 jsInstances l4i =
   let sctabs = scopetable l4i
   in
-  vvsep [ "//" <+> scopenameStr scopename <+> "scope" <//>
+  vvsep [ "//" <+> "scope" <+> scopenameStr scopename <//>
+          "//" <+> viaShow symtab' <//>
           vvsep [ "const" <+> snake_case mt <+> prettyMaybeType "ts" (snake_inner . MTT) (getSymType symtype) <+> equals <+> nest 2 value
                 | (mt, (symtype, vals)) <- Map.toList symtab'
                 , value <- case vals of
                               -- what we should do is gather all the paramtexts and join them in a single dictionary,
                               -- rather than assume that all the HC2 are paramtexts.
-                              HC { hHead = RPParamText {} } : _ -> [lbrace </> asValuePT l4i vals </>
-                                                                     rbrace <> Prettyprinter.line]
-                              _                                  -> asValue l4i <$> vals
+                              HC { hHead = RPParamText {} } : _ -> [ encloseSep lbrace (line <> rbrace) comma ((line <>) <$> asValuePT l4i vals) ]
+                              _                                 -> asValue l4i <$> vals
                  ]
         | (scopename , symtab') <- Map.toList sctabs
         ] </>
@@ -91,6 +94,26 @@ jsInstances l4i =
     scopenameStr [] = "globals"
     scopenameStr x  = snake_case x
 
+-- | top-level DEFINEs, going rule by rule
+-- just the facts.
+globalDefinitions :: Interpreted -> Doc ann
+globalDefinitions l4i =
+  vvsep [ "const" <+> dumpNestedClass l4i f
+        | f <- globalFacts l4i
+        ]
+
+  
+dumpNestedClass :: Interpreted -> DT.Tree ParamText -> Doc ann
+dumpNestedClass l4i (DT.Node pt children)
+  | not $ null children =
+    -- "// pt:"       <+> viaShow pt <//>
+    -- "// children:" <+> viaShow children <//>
+    pretty (PT5 pt l4i) <> nest 2 (encloseSep (line <> lbrace <> space) (line <> rbrace) (comma <> space) ( dumpNestedClass l4i <$> children ))
+  | otherwise =
+    -- "// pt:"       <+> viaShow pt <//>
+    pretty (PT4 pt l4i)
+
+
 -- [TODO] convert the GIVEN ... logic into functions that do the right thing.
 
 asValue :: Interpreted -> HornClause2 -> Doc ann
@@ -100,8 +123,8 @@ asValue _l4i _hc2@HC { hHead = RPBoolStructR mt1 _rprel _bsr } = snake_case mt1 
 asValue  l4i _hc2@HC { hHead = RPParamText pt }                = pretty (PT4 pt l4i)
 asValue  l4i  hc2@HC { hHead = RPnary      _rprel rp }         = asValue l4i hc2 {hHead = rp}
 
-asValuePT :: Interpreted -> [HornClause2] -> Doc ann
+asValuePT :: Interpreted -> [HornClause2] -> [Doc ann]
 asValuePT l4i hc2s = -- trace ("asValuePT: " <> show hc2s) $
-                     vsep [ pretty (PT4 pt l4i) <> comma
-                          | HC { hHead = RPParamText pt } <- hc2s ]
+  [ pretty (PT4 pt l4i)
+  | HC { hHead = RPParamText pt } <- hc2s ]
                  

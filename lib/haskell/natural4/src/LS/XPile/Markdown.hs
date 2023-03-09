fa65e480feb679ba4fecc66cc9620bde88741272
Meowyam <maryam@chilon.net>
2023-03-09 20:22:53 +0800

    filter out TComparison

--- lib/haskell/natural4/src/LS/XPile/Markdown.hs
+++ lib/haskell/natural4/src/LS/XPile/Markdown.hs
@@ -6,8 +6,9 @@
 module LS.XPile.Markdown where
 
 import LS
+import LS.Types
 import LS.NLP.NLG
-import AnyAll as AA
+import qualified AnyAll as AA
 import qualified Data.Map as Map
 import qualified Data.Text as Text
 -- import Debug.Trace (trace)
@@ -18,3 +19,30 @@ markdown env rl = do
   print $ concatMap Text.unpack nl
   print "---"
   return $ concatMap Text.unpack nl
+
+bsMarkdown :: [Rule] -> IO String
+bsMarkdown rl = do
+  let txt = Text.unwords $ bs rl
+  return $ Text.unpack txt
+
+rpFilter :: RelationalPredicate -> MultiTerm
+rpFilter (RPConstraint mt1 rel mt2) = mt1 ++ mt2
+rpFilter (RPBoolStructR mt1 rel bsr) = mt1 ++ bs2mt bsr
+rpFilter (RPnary rel rp) = rpFilter rp
+rpFilter x = rp2mt x
+
+rl2bs :: [Rule] -> [BoolStructR]
+rl2bs rl = map snd $ qaHornsR interp
+  where
+    interp = l4interpret defaultInterpreterOptions rl
+
+bs :: [Rule] -> [Text.Text]
+bs rl = map (mt2text . bs2mt) $ rl2bs rl
+
+bs2mt :: BoolStructR -> MultiTerm
+bs2mt (AA.All _ x)  = concatMap bs2mt x
+bs2mt (AA.Any _ x)  = concatMap bs2mt x
+bs2mt (AA.Not x)    =           bs2mt x
+bs2mt (AA.Leaf rp)  =           rpFilter rp
+
+-- Leaf (RPConstraint [MTT "Loss or Damage"] RPis [MTT "caused by",MTT "birds"])]

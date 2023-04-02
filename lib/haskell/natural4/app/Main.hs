cbd6d5e8bcb9c9a75a3844acb1faadbab21f61ba
Meng Weng Wong <mengwong@gmail.com>
2023-04-03 01:30:49 +0800

    truncating the svg filename was a bad idea

--- lib/haskell/natural4/app/Main.hs
+++ lib/haskell/natural4/app/Main.hs
@@ -149,7 +149,7 @@ main = do
                appendFile (dname <> "/index.html") ("<li> " <> "<a target=\"aasvg\" href=\"" <> fnamext <> "\">" <> displayTxt
                                                     <> "</a></li>\n")
            | (n,(svgtiny,svgfull,hsAnyAllTree,hsQtree)) <- Map.toList asaasvg
-           , let (fname, ext) = (take 20 (snakeScrub (SFL4.mtexpr2text <$> n)), "svg")
+           , let (fname, ext) = (take 128 (snakeScrub (SFL4.mtexpr2text <$> n)), "svg")
            ]
       myMkLink iso8601 (toaasvgFN <> "/" <> "LATEST")
 

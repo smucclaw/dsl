d068fe12c1cc46c08649e707ea094e02ac734684
Joe Watt <joewatt95@u.nus.edu>
2023-05-29 21:01:25 +0800

    More work on testcases

--- lib/haskell/natural4/src/LS/XPile/CoreL4.hs
+++ lib/haskell/natural4/src/LS/XPile/CoreL4.hs
@@ -10,6 +10,7 @@
 module LS.XPile.CoreL4
   ( sfl4ToBabyl4,
     sfl4ToCorel4,
+    sfl4ToLogicProgramStr,
     sfl4ToASP,
     sfl4ToEpilog,
     sfl4ToDMN,
--- lib/haskell/natural4/test/LS/XPile/CoreL4/LogicProgramSpec.hs
+++ lib/haskell/natural4/test/LS/XPile/CoreL4/LogicProgramSpec.hs
@@ -1,22 +1,28 @@
 {-# LANGUAGE GHC2021 #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS_GHC -Wno-missing-fields #-}
 
 module LS.XPile.CoreL4.LogicProgramSpec
   ( spec
   )
 where
 
+import Control.Monad (join)
+import Data.Bifunctor (Bifunctor (bimap))
+import Data.Char (isSpace)
 import Data.Map qualified as Map
-import Data.String.Interpolate (i)
-import Data.Text qualified as T
+import Flow ((|>))
+import LS (Rule)
 import LS qualified
 import LS.Lib (NoLabel (..), Opts (..))
-import LS.XPile.CoreL4 ( sfl4ToASP, sfl4ToEpilog )
-import LS.XPile.CoreL4.LogicProgram.Common ( LPLang )
+import LS.XPile.CoreL4 (sfl4ToASP, sfl4ToEpilog, sfl4ToLogicProgramStr)
+import LS.XPile.CoreL4.LogicProgram.Common (LPLang (..))
 import Options.Generic (Unwrapped)
+import System.Directory
 import System.FilePath
 import System.FilePath.Find as Find
 import Test.Hspec
@@ -27,7 +33,6 @@ import Test.Hspec
     pending,
     shouldBe,
   )
-import System.Directory
 
 -- testCsvFiles :: Opts Unwrapped
 -- testCsvFiles = Opts {..}
@@ -39,31 +44,65 @@ import System.Directory
 --     dbug = False
 --     dstream = False
 
-testCasesDir :: FilePath
-testCasesDir = "test" </> "TestCases" </> "LogicProgram"
-
-data LPTestCase = LPTestCase
-  { dirName :: T.Text,
-    natural4InputFile :: T.Text,
-    expectedOutputs :: Map.Map LPLang T.Text
-  }
-  deriving (Eq, Ord, Read, Show)
-
 spec :: Spec
 spec = do
   describe "ASP transpiler" $ do
     it "Motor insurance" $ do
-      csvFile : _ <- Find.find always (extension ==? ".csv") (testCasesDir </> "motor-insurance")
+    --   csvFile : _ <- Find.find always (extension ==? ".csv") (testcasesDir </> "motor-insurance")
 
-      let file = NoLabel [csvFile]
-          dbug = False
-          dstream = False
-          opts :: Opts Unwrapped = Opts {..}
+    --   let file = NoLabel [csvFile]
+    --       dbug = False
+    --       dstream = False
+    --       opts :: Opts Unwrapped = Opts {..}
     
-      rules <- LS.dumpRules opts
+    --   rules <- LS.dumpRules opts
+    --   rules |> sfl4ToASP |> filter (not . isSpace) |> print
       -- print $ sfl4ToASP rules
       pending
 
   describe "Epilog transpiler" $ do
     it "pending"
       pending
+
+data LPTestcase = LPTestCase
+  { dirName :: String,
+    csvFile :: String,
+    expectedOutputFiles :: Map.Map LPLang String
+  }
+  deriving (Eq, Ord, Read, Show)
+
+testcase2specs :: LPTestcase -> Map.Map LPLang Spec
+testcase2specs testcase = Map.fromList $ do
+  lpLang <- [ASP, Epilog]
+  pure (lpLang, testcase2spec lpLang testcase)
+
+testcase2spec :: LPLang -> LPTestcase -> Spec
+testcase2spec lpLang LPTestCase {..} =
+  it dirName $ do
+    let findFile fileName' =
+          Find.find always (fileName ==? fileName') (testcasesDir </> dirName)
+        testcasesDir = "test" </> "Testcases" </> "LogicProgram"
+        expectedOutputFile =
+          expectedOutputFiles |> Map.lookup lpLang |> maybe "" show
+        xpileFn = case lpLang of
+          ASP -> sfl4ToASP
+          Epilog -> sfl4ToEpilog
+
+    csvFile : _ <- findFile csvFile
+    rules :: [Rule] <-
+      LS.dumpRules
+        Opts
+          { file = NoLabel [csvFile],
+            dbug = False,
+            dstream = False
+          }
+
+    expectedOutputFile : _ <- findFile expectedOutputFile
+    expectedOutput <- readFile expectedOutputFile
+
+    let (xpiled, expectedOutput) =
+          (xpileFn rules, expectedOutput)
+            |> join bimap removeSpaces
+        removeSpaces = filter $ not . isSpace
+
+    xpiled `shouldBe` expectedOutput

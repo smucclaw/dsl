e214c8ca9c44bde9ceb3a8bbdee65d9855019f8f
joewatt95 <joewatt95@u.nus.edu>
2023-05-29 14:03:35 +0800

    Basic setup for ASP and Epilog xpile test cases

---
+++ lib/haskell/natural4/test/LS/XPile/CoreL4/LogicProgramSpec.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE GHC2021 #-}
+-- {-# LANGUAGE OverloadedStrings #-}
+
+module LS.XPile.CoreL4.LogicProgramSpec
+  ( spec
+  )
+where
+
+import Test.Hspec
+  ( Spec,
+    describe,
+    it,
+    pending,
+    shouldBe,
+  )
+
+spec :: Spec
+spec = do
+  describe "ASP transpiler" $ do
+    it "pending"
+      pending
+
+  describe "Epilog transpiler" $ do
+    it "pending"
+      pending

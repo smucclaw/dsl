39a38da3c4bfb0870f90d620886e95c3bc72c434
joewatt95 <joewatt95@u.nus.edu>
2023-05-30 11:28:06 +0800

    Add motor insurance test case (currently failing)

--- lib/haskell/natural4/test/LS/XPile/CoreL4/LogicProgramSpec.hs
+++ lib/haskell/natural4/test/LS/XPile/CoreL4/LogicProgramSpec.hs
@@ -4,7 +4,6 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# OPTIONS_GHC -Wno-missing-fields #-}
 
 module LS.XPile.CoreL4.LogicProgramSpec
   ( spec
@@ -15,12 +14,14 @@ import Control.Monad (join)
 import Data.Bifunctor (Bifunctor (bimap, first))
 import Data.Char (isSpace)
 import Data.HashMap.Strict qualified as HM
-import Data.Maybe (listToMaybe)
+import Data.HashSet qualified as HS
+import Data.Hashable (Hashable)
+import Data.Maybe (listToMaybe, fromMaybe)
 import Flow ((|>))
+import GHC.Generics (Generic)
 import LS (Rule)
 import LS qualified
 import LS.Lib (NoLabel (..), Opts (..))
-import LS.Utils ((|$>))
 import LS.XPile.CoreL4 (sfl4ToASP, sfl4ToEpilog, sfl4ToLogicProgramStr)
 import LS.XPile.CoreL4.LogicProgram.Common (LPLang (..))
 import Options.Generic (Unwrapped)
@@ -46,32 +47,42 @@ import Test.Hspec
 --     dbug = False
 --     dstream = False
 
+testcases :: [LPTestcase]
+testcases =
+  [ LPTestcase
+      { dir = "motor-insurance",
+        csvFile = "motor-insurance.csv",
+        expectedOutputFiles = [(ASP, "asp.lp"), (Epilog, "epilog.lp")]
+      }
+  ]
+
 spec :: Spec
 spec = do
   describe "ASP transpiler" $ do
-    it "Motor insurance" $ do
+    testcase2spec ASP (head testcases)
     --   csvFile : _ <- Find.find always (extension ==? ".csv") (testcasesDir </> "motor-insurance")
 
     --   let file = NoLabel [csvFile]
     --       dbug = False
     --       dstream = False
     --       opts :: Opts Unwrapped = Opts {..}
-    
+
     --   rules <- LS.dumpRules opts
     --   rules |> sfl4ToASP |> filter (not . isSpace) |> print
       -- print $ sfl4ToASP rules
-      pending
 
   describe "Epilog transpiler" $ do
     it "pending"
       pending
 
-data LPTestcase = LPTestCase
+data LPTestcase = LPTestcase
   { dir :: FilePath,
     csvFile :: FilePath,
     expectedOutputFiles :: HM.HashMap LPLang FilePath
   }
-  deriving (Eq, Ord, Read, Show)
+  deriving (Eq, Generic, Ord, Read, Show)
+
+instance Hashable LPTestcase
 
 testcase2specs :: LPTestcase -> HM.HashMap LPLang Spec
 testcase2specs testcase = HM.fromList $ do
@@ -79,15 +90,14 @@ testcase2specs testcase = HM.fromList $ do
   pure (lpLang, testcase2spec lpLang testcase)
 
 testcase2spec :: LPLang -> LPTestcase -> Spec
-testcase2spec lpLang LPTestCase {..} =
+testcase2spec lpLang LPTestcase {..} =
   it dir $ do
     let findFile :: FilePath -> IO (Maybe FilePath)
         findFile file =
-          Find.find always (fileName ==? file) (testcasesDir </> dir)
-            |$> listToMaybe
-        testcasesDir :: FilePath = "test" </> "Testcases" </> "LogicProgram"
-        expectedOutputFile :: String =
-          expectedOutputFiles |> HM.lookup lpLang |> maybe "" show
+          listToMaybe <$> Find.find always (fileName ==? file) dir'
+        dir' :: FilePath = "test" </> "Testcases" </> "LogicProgram" </> dir
+        expectedOutputFile :: FilePath =
+          expectedOutputFiles |> HM.lookup lpLang |> fromMaybe ""
         xpileFn :: [Rule] -> String =
           case lpLang of
             ASP -> sfl4ToASP
@@ -101,6 +111,11 @@ testcase2spec lpLang LPTestCase {..} =
             dbug = False,
             dstream = False
           }
+
+    -- print $ expectedOutputFile
+
+    -- Find.find always (fileName ==? "asp.lp") dir'
+    --   >>= print
 
     Just expectedOutputFile <- findFile expectedOutputFile
     expectedOutput :: String <- readFile expectedOutputFile

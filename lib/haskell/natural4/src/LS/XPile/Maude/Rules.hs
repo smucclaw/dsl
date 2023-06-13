7609764a5db26e5af04ec2b21a5e871b0ee2f99f
joewatt95 <joewatt95@u.nus.edu>
2023-06-13 14:34:59 +0800

    cleanup Rules.hs

--- lib/haskell/natural4/src/LS/XPile/Maude/Rules.hs
+++ lib/haskell/natural4/src/LS/XPile/Maude/Rules.hs
@@ -11,22 +11,23 @@ module LS.XPile.Maude.Rules
   )
 where
 
+import Control.Monad.Validate (runValidate)
+import Data.Bifunctor (Bifunctor (..))
 import Data.Coerce (coerce)
 import Data.Either (rights)
 import Data.Foldable qualified as Fold
 import Data.Maybe (mapMaybe)
 import Data.MonoTraversable (Element, otoList)
+import Data.Monoid (Ap (..))
 import Data.Sequences as Seq (IsSequence)
+import Data.Text qualified as T
 import Flow ((|>))
 import LS.Rule (Rule (..))
-import LS.Utils ((|$>), swallowErrs, runMonoidValidate)
+import LS.Utils (MonoidValidate, mapThenSwallowErrs, runMonoidValidate, (|$>))
 import LS.XPile.Maude.Rule (rule2doc)
 import LS.XPile.Maude.Utils (text2qid)
 import Prettyprinter (Doc, concatWith)
 import Prettyprinter.Interpolate (di)
-import Control.Monad.Validate (runValidate)
-import Data.Monoid (Ap (..))
-import Data.Bifunctor (Bifunctor(..))
 
 -- Main function to transpile rules to plaintext natural4 for Maude.
 rules2maudeStr :: (IsSequence t, Element t ~ Rule) => t -> String
@@ -42,25 +43,27 @@ rules2doc (otoList -> rules :: [Rule]) =
     -- Don't just swallow up errors and turn them into mempty.
     -- Actually output a comment indicating what went wrong while transpiling
     -- those erraneous rules.
-  concatWith (<.>) $ startRule <> (transpiledRules |$> snd |> swallowErrs)
+  concatWith (<.>) $ startRule <> transpiledRules
   where
     -- Find the name of the first regulative rule which got transpiled correctly.
     -- If such a rule exists, we turn it into a quoted symbol and prepend START.
     startRule :: [Doc ann] =
-      transpiledRules
-        |> mapMaybe (
-            \case
-              ( Regulative {rlabel = Just (_, _, ruleName)},
-                runMonoidValidate -> Right _
-               ) -> Just ruleName
-              _ -> Nothing
-          )
+      transpiledRulesPairs
+        |> mapMaybe validRegRuleToRuleName
         |> take 1
         |$> \ruleName -> [di|START #{text2qid ruleName}|]
 
     -- Transpile all rules to plaintext, keeping track of the original rule.
-    transpiledRules = do
-      rule <- rules
-      pure (rule, rule2doc rule)
+    transpiledRulesPairs = [(rule, rule2doc rule) | rule <- rules]
+
+    transpiledRules = mapThenSwallowErrs snd transpiledRulesPairs
+
+    validRegRuleToRuleName ::
+      (Rule, MonoidValidate e a) -> Maybe T.Text
+    validRegRuleToRuleName
+      ( Regulative {rlabel = Just (_, _, ruleName)},
+        runMonoidValidate -> Right _
+        ) = Just ruleName
+    validRegRuleToRuleName _ = Nothing
 
     x <.> y = [di|#{x},\n\n#{y}|]

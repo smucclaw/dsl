aeced883b1ba20a9850ff9ac27e0148967621746
ymh <hanyongming@gmail.com>
2023-09-14 01:20:07 +0800

    start of utils for providing english-y translations of optics abstractions over concrete data structures

---
+++ lib/haskell/natural4/src/LS/XPile/LogicalEnglish/Utils.hs
@@ -0,0 +1,31 @@
+{-# OPTIONS_GHC -W #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
+-- {-# LANGUAGE LambdaCase #-}
+-- {-# LANGUAGE OverloadedRecordDot, DuplicateRecordFields, RecordWildCards, NoFieldSelectors #-}
+-- {-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE TypeFamilies #-}
+-- {-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+
+module LS.XPile.LogicalEnglish.Utils (setInsert) where
+
+-- import Data.Text qualified as T
+import Data.HashSet qualified as HS
+-- import Data.Hashable (Hashable, hashWithSalt, hashUsing)
+import Optics
+
+{- Optics provide a nice interface over concrete data structures,
+but they can sometimes be hard to read / understand.
+The following functions provide more readable variants of those functions
+-}
+
+
+{- | Insert into __any__ kind of set
+
+Examples:
+
+>>> setInsert 5 (HS.fromList [1..3])
+fromList [1,2,3,5]
+-}
+setInsert :: forall {a}. (IxValue a ~ (), At a) => Index a -> a -> a
+elt `setInsert` set = set & at' elt ?~()

4a68399082c2707576d6aca38bcfc8cd6fcaaaa0
joewatt95 <joewatt95@u.nus.edu>
2023-09-13 15:52:32 +0800

    Revamp testing of LE transpiler

--- lib/haskell/natural4/test/LS/XPile/LogicalEnglish/LogicalEnglishSpec.hs
+++ lib/haskell/natural4/test/LS/XPile/LogicalEnglish/LogicalEnglishSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE ExplicitNamespaces #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -16,14 +17,23 @@
 --      - is-num
 --          - is-num.csv
 --          - expected.le
---          - description.txt
+--          - config.yml
 --  where:
 --  - 'is-num.csv' is the name of the input natural4 csv file.
 --  - 'expected.le' is a file containing the expected Logical English output.
---  - 'description.txt' is a file containing a description of the unit test.
+--  - 'condig.yml' is a YAML configuration file for the unit test.
 --
 --  Note that:
---  - The file names 'expected.le' and 'description.txt' are hardcoded
+--  - 'config.yml' should look something like:
+--      description: "If IS is followed by a number, number should become *a number*"
+--      enabled: true
+--  - ie it should contain a string field called 'description', which gives
+--    the description of the test case found in that directory, and
+--    'enabled' is a boolean (ie 'true' or 'false') indicating if the test
+--    case is currently enabled.
+--    Test cases which are not enabled are skipped automatically by this module.
+--
+--  - The file names 'expected.le' and 'config.yml' are hardcoded
 --    and so should not be varied
 --  - The name of the parent directory and csv file can be changed, but should be
 --    kept in sync with each other, ie if the parent directory is 'abc', then
@@ -41,44 +51,77 @@
 module LS.XPile.LogicalEnglish.LogicalEnglishSpec (spec) where
 
 import Data.Foldable (for_)
-import Data.String.Interpolate (__i)
+import Data.Maybe (listToMaybe)
+import Data.String.Interpolate (i)
 import Data.Text qualified as T
 import Data.Text.IO qualified as TIO
 import Data.Yaml qualified as Yaml
 import Flow ((|>))
 import GHC.Generics (Generic)
 import LS (Rule)
+import LS.Utils ((|$>))
 import LS.XPile.LogicalEnglish (toLE)
 import LS.XPile.LogicalEnglish.GoldenUtils (goldenLE)
 import LS.XPile.LogicalEnglish.UtilsLEReplDev (leTestcasesDir, letestfnm2rules)
-import System.FilePath ((<.>), (</>))
-import Test.Hspec (Spec, describe, it, runIO)
+import System.FilePath (takeBaseName, (<.>), (</>))
+import System.FilePath.Find
+  ( FileType (Directory),
+    depth,
+    extension,
+    fileType,
+    (<=?),
+    (==?), fileName,
+  )
+import System.FilePath.Find qualified as FileFind
+import Test.Hspec (Spec, describe, it, pendingWith, runIO)
 
 -- | The 'Spec' used to test the Logical English transpiler.
 spec :: Spec
 spec = describe "Logical English" $ do
-  testcaseDirs :: Either Yaml.ParseException [FilePath] <-
-    leTestcasesDir </> "testcases.yml"
-      |> Yaml.decodeFileEither
+  testcaseDirs :: [FilePath] <-
+    leTestcasesDir
+      |>  findWithDepth0 (fileType ==? Directory)
+      |$> tail
       |> runIO
-  testcaseDirs |> either (runIO . reportYamlError) runTestcases
-
-runTestcases :: Foldable t => t FilePath -> Spec
-runTestcases testcaseDirs =
   for_ testcaseDirs $ \testcaseDir -> do
-    let descrFile = "description" <.> "txt"
-        l4csvFile = testcaseDir <.> "csv"
-    description <- runIO $ readFile $
-                    leTestcasesDir </> testcaseDir </> descrFile
-    it description $ do
-      l4rules <- letestfnm2rules l4csvFile
-      let leProgram = l4rules |> toLE |> T.pack
-      pure $ goldenLE testcaseDir leProgram
+    configFile :: Maybe FilePath <-
+      testcaseDir
+        |> findWithDepth0 (fileName ==? "config.yml")
+        |$> listToMaybe
+        |> runIO
+    case configFile of
+      Nothing -> it testcaseDir $ pendingWith "Missing config.yml file."
+      Just configFile -> testcaseConfigFile2spec testcaseDir configFile
 
-reportYamlError :: Yaml.ParseException -> IO ()
-reportYamlError parseExc =
-  TIO.putStrLn
-    [__i|
-      Skipping Logical English tests because an error occured while reading testcases.yml:
-      #{parseExc}
-    |]
+testcaseConfigFile2spec :: FilePath -> FilePath -> Spec
+testcaseConfigFile2spec testcaseDir configFile = do
+  testcaseConfig <- configFile |> Yaml.decodeFileEither |> runIO
+  case testcaseConfig of
+    Left parseExc ->
+      it testcaseDir $ pendingWith
+        [i|Error occured while parsing Yaml file: #{parseExc}|]
+    Right testcaseConfig -> testcaseConfig2spec testcaseDir testcaseConfig
+
+testcaseConfig2spec :: FilePath -> TestcaseConfig -> Spec
+testcaseConfig2spec testcaseDir TestcaseConfig {..} =
+  describe testcaseDir $
+    if enabled
+      then it description $ do
+        l4rules <- letestfnm2rules l4csvFile
+        let leProgram = l4rules |> toLE |> T.pack
+        pure $ goldenLE testcaseDir leProgram
+      else it description $ pendingWith "Test case is disabled."
+  where
+    descrFile = "description" <.> "txt"
+    l4csvFile = takeBaseName testcaseDir <.> "csv"
+
+findWithDepth0 :: FileFind.FilterPredicate -> FilePath -> IO [FilePath]
+findWithDepth0 = FileFind.find (depth ==? 0) 
+
+data TestcaseConfig = TestcaseConfig
+  { description :: String,
+    enabled :: Bool
+  }
+  deriving (Generic, Show)
+
+instance Yaml.FromJSON TestcaseConfig

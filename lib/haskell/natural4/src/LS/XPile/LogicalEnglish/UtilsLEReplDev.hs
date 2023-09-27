c36cd1fd09f974d196369a7e9da6728fee682a3f
ymh <hanyongming@gmail.com>
2023-09-27 22:17:44 +0800

    add doctests infra for NatL4 (via `doctest-parallel`) and correct existing doctests

--- lib/haskell/natural4/src/LS/XPile/LogicalEnglish/UtilsLEReplDev.hs
+++ lib/haskell/natural4/src/LS/XPile/LogicalEnglish/UtilsLEReplDev.hs
@@ -45,18 +45,15 @@ prl = pRules leTestcasesDir
 leTestCSVs :: IO [FilePath]
 leTestCSVs = csvsInDir leTestcasesDir
 
+{- | IO action that gets you a list of basefilenms of .csvs in the testcases dir or any subdir of it
 
+The output will look like
+
+["and-not.csv","date-time.csv","ditto.csv","ditto-with-spaces.csv","filtering-will-not-remove-all-nlas-in-eq-class.csv","x-is-not-y.csv","indentation-propn-databreach.csv","is-in.csv","is-max-terms-simple.csv","is-min-terms-simple.csv","is-not-diff-from.csv","is-num-simple.csv","or-anaphora-with-ditto.csv","product-twiceof.csv","rpc-rpeq-year.csv","rpnary-is-max-x-where.csv","rpnary-is-min-x-where.csv","parentchild-with-period.csv","sum-of-terms-simple.csv","template-regex-filters-out-subbed-phrases.csv","template-regex-is-anchored-start-and-end.csv","vacuous-no-l4-rules.csv"]
+-}
 testfnmsLE :: IO [BaseFileName]
 testfnmsLE = (fmap . fmap) takeFileName (csvsInDir leTestcasesDir)
-{-
-["and-not.csv",
-"indentation-databreach.csv",
-"product.csv",
-"rpnary-and.csv",
-"parentchild-with-period.csv",
-"parentchild-without-period.csv",
-"simple-sum.csv"]
--}
+
 
 letestfnm2rules :: BaseFileName -> IO [Rule]
 letestfnm2rules = l4csv2rules leTestcasesDir
@@ -73,5 +70,3 @@ motorrules = l4csv2rules ("test" </> "Testcases") "motor-insurance-1.csv"
 
 indentpdparules :: IO [Rule]
 indentpdparules = letestfnm2rules "indentation-databreach.csv" 
-
--- withnumsrules = letestfnm2rules "with-numbers.csv"

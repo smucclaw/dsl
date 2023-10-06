2655c696fc383b724e1c2c91a886d829b837209f
ymh <hanyongming@gmail.com>
2023-10-06 17:54:36 +0800

    LE: Have decimals use fixed / standard instead of scientific notation

--- lib/haskell/natural4/src/LS/XPile/LogicalEnglish/SimplifyL4.hs
+++ lib/haskell/natural4/src/LS/XPile/LogicalEnglish/SimplifyL4.hs
@@ -16,9 +16,9 @@ module LS.XPile.LogicalEnglish.SimplifyL4 (simplifyL4rule, SimpL4(..), SimL4Erro
 
 import Data.Text qualified as T
 import qualified Data.Text.Lazy as T (toStrict)
-import qualified Data.Text.Lazy.Builder as B
+import qualified Data.Text.Lazy.Builder as B 
 import qualified Data.Text.Lazy.Builder.Int as B
-import qualified Data.Text.Lazy.Builder.RealFloat as B
+import qualified Data.Text.Lazy.Builder.RealFloat as B (FPFormat(..), formatRealFloat)
 
 import Control.Monad.Validate
   ( MonadValidate (..)
@@ -431,7 +431,15 @@ mtes2cells = fmap mte2cell
 Thanks to Jo Hsi for finding these!
 -}
 float2Text :: RealFloat a => a -> T.Text
-float2Text = T.toStrict . B.toLazyText . B.realFloat
+float2Text = T.toStrict . B.toLazyText . decFloat
+
+{- | Differs from B.realFloat only in that we use standard decimal notation (i.e., in the choice of FPFormat)
+See https://hackage.haskell.org/package/text-2.1/docs/src/Data.Text.Lazy.Builder.RealFloat.html
+-}
+decFloat :: (RealFloat a) => a -> B.Builder
+{-# SPECIALIZE decFloat :: Float -> B.Builder #-}
+{-# SPECIALIZE decFloat :: Double -> B.Builder #-}
+decFloat x = B.formatRealFloat B.Fixed Nothing x
 
 int2Text :: Integral a => a -> T.Text
 int2Text = T.toStrict . B.toLazyText . B.decimal

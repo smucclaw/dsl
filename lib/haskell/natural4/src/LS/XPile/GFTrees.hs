9be9ddf154889a748b713dca4680e69487cc8f22
Joe Watt <joewatt95@u.nus.edu>
2023-12-04 12:55:53 +0800

    remove another import, seq fmap -> traverse

--- lib/haskell/natural4/src/LS/XPile/GFTrees.hs
+++ lib/haskell/natural4/src/LS/XPile/GFTrees.hs
@@ -9,7 +9,6 @@ Largely a wrapper. Most of the functionality is in the anyall lib.
 module LS.XPile.GFTrees where
 
 import AnyAll qualified as AA
-import Data.Map qualified as Map
 import Data.Maybe (catMaybes, listToMaybe, mapMaybe)
 import Data.Text qualified as T
 import LS
@@ -22,7 +21,7 @@ import Control.Monad (mapM, join)
 
 
 gftrees :: NLGEnv -> [Rule] -> XPileLog [BoolStructGText]
-gftrees env rl = join <$> sequence (ruleQnTrees env alias <$> rl)
+gftrees env rl = join <$> traverse (ruleQnTrees env alias) rl
   where
     alias = listToMaybe [(you,org) | DefNameAlias you org _ _ <- rl]
 

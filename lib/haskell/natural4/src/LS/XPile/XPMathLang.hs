0b82f77cdc00d613ca3ad6ca7bb5b9b45eb88323
Meng Weng Wong <mengwong@gmail.com>
2023-12-18 14:02:12 +0800

    beginnings of a 'transpiler" to MathLang

---
+++ lib/haskell/natural4/src/LS/XPile/XPMathLang.hs
@@ -0,0 +1,42 @@
+module LS.XPile.XPMathLang where
+
+import LS.Interpreter
+import Explainable.MathLang
+
+-- | calling the output "MyState" is misleading, but this is the most general way to cover the idea that
+-- a ruleset consists of more than one rule, similar to how the Vue interface gives more than one
+-- element in the left nav; each of the different rules will have its own entry in the SymTab dictionary.
+--
+-- so, for example, if we have a single MustSing input, we would expect the output MyState dictionary symtab
+-- to contain an @Expr Float@ called "must sing"
+asMathlang :: Interpreted -> MyState
+
+asMathLang l4i =
+  MyState { symtabF = Map.fromList [("must sing", Val 1.0)] }
+
+-- we want to read the rules from Interpreted and transform them into MathLang expressions.
+-- so for example, if the L4 says
+-- DECIDE foo
+--     IF bar
+--    AND baz
+--    AND quux
+--
+-- we would want the MathLang output to be
+-- output :: Pred Float
+-- output = PredBin Nothing PredAnd (PredVar "foo")
+--                                  (PredBin Nothing PredAnd (PredVar "bar")
+--                                                           (PredBin Nothing PredAnd (PredVar "baz")
+--                                                                                    (PredVar "quux")))
+--
+-- DECIDE foo
+--     IF      ALL   bar
+--                   baz
+--                   quux
+--
+-- output :: Pred Float
+-- output = PredFold Nothing PLAnd [PredVar "foo"
+--                                 ,PredVar "bar"
+--                                 ,PredVar "baz"
+--                                 ,PredVar "quux"]
+)
+

774fc54c42624006cca576eb7afe016d7613a9c0
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2023-12-19 09:06:53 +0800

    Make it compile

--- lib/haskell/natural4/src/LS/XPile/XPMathLang.hs
+++ lib/haskell/natural4/src/LS/XPile/XPMathLang.hs
@@ -2,6 +2,8 @@ module LS.XPile.XPMathLang where
 
 import LS.Interpreter
 import Explainable.MathLang
+import LS.Rule (Interpreted)
+import Data.HashMap.Strict qualified as Map
 
 -- | calling the output "MyState" is misleading, but this is the most general way to cover the idea that
 -- a ruleset consists of more than one rule, similar to how the Vue interface gives more than one
@@ -9,10 +11,10 @@ import Explainable.MathLang
 --
 -- so, for example, if we have a single MustSing input, we would expect the output MyState dictionary symtab
 -- to contain an @Expr Float@ called "must sing"
-asMathlang :: Interpreted -> MyState
+asMathLang :: Interpreted -> MyState
 
 asMathLang l4i =
-  MyState { symtabF = Map.fromList [("must sing", Val 1.0)] }
+  MyState { symtabF = Map.fromList [("must sing", Val Nothing 1.0)] }
 
 -- we want to read the rules from Interpreted and transform them into MathLang expressions.
 -- so for example, if the L4 says
@@ -38,5 +40,3 @@ asMathLang l4i =
 --                                 ,PredVar "bar"
 --                                 ,PredVar "baz"
 --                                 ,PredVar "quux"]
-)
-

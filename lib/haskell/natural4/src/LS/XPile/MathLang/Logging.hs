6abea566a53ae9283b76d42f99278d7070d5ab13
ymh <hanyongming@gmail.com>
2024-01-24 15:26:39 +0800

    WIP: Stub out a prelim Logging.hs for MathLang xpiler

---
+++ lib/haskell/natural4/src/LS/XPile/MathLang/Logging.hs
@@ -0,0 +1,56 @@
+{-# OPTIONS_GHC -W #-}
+{-# OPTIONS_GHC -foptimal-applicative-do #-}
+
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedRecordDot, DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DataKinds, KindSignatures, AllowAmbiguousTypes, ApplicativeDo #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE PatternSynonyms, ViewPatterns, DataKinds, GADTs #-}
+{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+
+
+{- VERY WIP! more of a stub / outline rn -}
+module LS.XPile.MathLang.Logging where
+-- TODO: Add export list
+
+import qualified Data.Time as Time
+import Log (LogT, Logger, defaultLogLevel, object, (.=))
+import qualified Log
+import qualified Log.Backend.StandardOutput as Log
+
+import Effectful
+import Effectful.Error.Dynamic 
+import Effectful.Reader.Dynamic 
+
+-- import Optics ((^.))
+-- import Data.Generics.Product.Types (types)
+-- import Data.String.Interpolate (i)
+
+-- import Data.HashSet qualified as HS
+-- import Data.Hashable (Hashable)
+import GHC.Generics (Generic)
+
+-- import Data.String (IsString)
+-- import Prettyprinter (Pretty)
+
+
+
+-- TODO: upstream logging stuff later
+data LoggingDestination
+  = -- | Logs are printed on the standard output
+    StdOut
+  | -- | Logs are printed on the standard output in JSON format
+    Json
+  | -- | Logs are sent to a file as JSON
+    JSONFile
+  deriving (Show, Generic)
+
+
+data LogCfg = MkLogCfg 
+  { logDest :: LoggingDestination
+  }
+
+

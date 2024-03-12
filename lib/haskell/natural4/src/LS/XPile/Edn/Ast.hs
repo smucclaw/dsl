3e65c2d2babc9237b6b57fb072f34b3115d4790b
Joe Watt <joewatt95@u.nus.edu>
2024-03-12 16:05:18 +0800

    cleanup

--- lib/haskell/natural4/src/LS/XPile/Edn/Ast.hs
+++ lib/haskell/natural4/src/LS/XPile/Edn/Ast.hs
@@ -1,14 +1,10 @@
 {-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module LS.XPile.Edn.Ast
@@ -26,37 +22,46 @@ import Data.Either (rights)
 import Data.String.Interpolate (i)
 
 data AstNode metadata
-  = Parenthesised {metadata :: Maybe metadata, children :: [AstNode metadata]}
-  | List {metadata :: Maybe metadata, elements :: [AstNode metadata]}
-  | Text {metadata :: Maybe metadata, text :: T.Text}
-  | Variable {metadata :: Maybe metadata, variable :: T.Text}
-  | Not {metadata :: Maybe metadata, negated :: AstNode metadata}
+  = RuleFact
+      { metadata :: Maybe metadata,
+        head :: AstNode metadata,
+        body :: Maybe (AstNode metadata)
+      }
   | AndOr
       { metadata :: Maybe metadata,
         andOr :: T.Text,
         children :: [AstNode metadata]
       }
-  | Rule
-      { metadata :: Maybe metadata,
-        head :: AstNode metadata,
-        body :: AstNode metadata
-      }
-  -- deriving (Eq, Ord, Read, Show, Generic, Hashable)
+  | Parens {metadata :: Maybe metadata, children :: [AstNode metadata]}
+  | List {metadata :: Maybe metadata, elements :: [AstNode metadata]}
+  | Text {metadata :: Maybe metadata, text :: T.Text}
+  | Variable {metadata :: Maybe metadata, variable :: T.Text}
+  deriving Show
 
 makeBaseFunctor ''AstNode
 
 astToEdn :: AstNode metadata -> T.Text
 astToEdn = EDN.renderText . cata \case
-  ParenthesisedF _ nodes -> EDN.toEDN nodes
-  TextF _ text -> toSymbol text
-  VariableF _ variable -> EDN.NoTag $ EDN.Symbol "var" variable
+  RuleFactF _ head body ->
+    [edn|DECIDE|] : head : ifBody |> EDN.toEDN
+    where
+      ifBody = case body of
+        Just body -> [[edn|IF|], body]
+        Nothing -> []
+
   AndOrF _ andOr children ->
     children |> intersperse (toSymbol andOr) |> EDN.toEDN
-  NotF _ node -> EDN.toEDN [[edn|NOT|], node]
-  RuleF _ head body -> EDN.toEDN [[edn|DECIDE|], head, [edn|IF|], body]
+
+  ParensF _ nodes -> EDN.toEDN nodes
+
   ListF _ rules -> rules |> EDN.mkVec |> EDN.toEDN
+
+  TextF _ text -> toSymbol text
+
+  VariableF _ variable -> toPrefixedSymbol "var" variable
   where
-    toSymbol x = EDN.Symbol "" [i|#{x}|] |> EDN.NoTag
+    toPrefixedSymbol prefix x = EDN.Symbol prefix [i|#{x}|] |> EDN.toEDN
+    toSymbol = toPrefixedSymbol ""
 
 pattern Number :: Maybe metadata -> Double -> AstNode metadata
 pattern Number metadata number <-
@@ -65,15 +70,25 @@ pattern Number metadata number <-
     Number metadata number = Text metadata [i|#{number}|]
 
 pattern Integer :: Maybe metadata -> Integer -> AstNode metadata
-pattern Integer metadata number <-
-  Text metadata (TRead.decimal -> Right (number, ""))
+pattern Integer metadata int <-
+  Text metadata (TRead.decimal -> Right (int, ""))
   where
-    Integer metadata number = Text metadata [i|#{number}|]
+    Integer metadata int = Text metadata [i|#{int}|]
 
 pattern Date :: Maybe metadata -> Integer -> Integer -> Integer -> AstNode metadata
 pattern Date metadata year month day =
-  Parenthesised
+  Parens
     metadata [Int year, Dash, Int month, Dash, Int day]
+
+pattern PrefixOp :: Maybe metadata -> T.Text -> [AstNode metadata] -> AstNode metadata
+pattern PrefixOp metadata op args =
+  Parens metadata [Text Nothing op, Parens Nothing args]
+
+pattern UnaryOp :: Maybe metadata -> T.Text -> AstNode metadata -> AstNode metadata
+pattern UnaryOp metadata op arg = PrefixOp metadata op [arg]
+
+pattern Not :: Maybe metadata -> AstNode metadata -> AstNode metadata
+pattern Not metadata negated = UnaryOp metadata "NOT" negated
 
 pattern Int :: Integer -> AstNode metadata
 pattern Int int = Integer Nothing int
@@ -82,7 +97,10 @@ pattern Dash :: AstNode metadata
 pattern Dash = Text Nothing "-"
 
 pattern Fact :: Maybe metadata -> AstNode metadata -> AstNode metadata
-pattern Fact metadata head = Rule metadata head (Parenthesised Nothing [])
+pattern Fact metadata head = RuleFact metadata head Nothing
+
+pattern Rule :: Maybe metadata -> AstNode metadata -> AstNode metadata -> AstNode metadata
+pattern Rule metadata head body = RuleFact metadata head (Just body)
 
 pattern Program :: Maybe metadata -> [AstNode metadata] -> AstNode metadata
 pattern Program metadata rules = List metadata rules
@@ -93,24 +111,24 @@ pattern And metadata conjuncts = AndOr metadata "AND" conjuncts
 pattern Or :: Maybe metadata -> [AstNode metadata] -> AstNode metadata
 pattern Or metadata conjuncts = AndOr metadata "OR" conjuncts
 
-pattern BinOp :: T.Text -> Maybe metadata -> AstNode metadata -> AstNode metadata -> AstNode metadata
-pattern BinOp binOp metadata lhs rhs =
-  Parenthesised metadata [lhs, Text Nothing binOp, rhs]
+pattern InfixBinOp :: Maybe metadata -> T.Text -> AstNode metadata -> AstNode metadata -> AstNode metadata
+pattern InfixBinOp metadata binOp lhs rhs =
+  Parens metadata [lhs, Text Nothing binOp, rhs]
 
 pattern Is :: Maybe metadata -> AstNode metadata -> AstNode metadata -> AstNode metadata
-pattern Is metadata lhs rhs = BinOp "IS" metadata lhs rhs
+pattern Is metadata lhs rhs = InfixBinOp metadata "IS" lhs rhs
 
 pattern Lt :: Maybe metadata -> AstNode metadata -> AstNode metadata -> AstNode metadata
-pattern Lt metadata lhs rhs = BinOp "<" metadata lhs rhs
+pattern Lt metadata lhs rhs = InfixBinOp metadata "<" lhs rhs
 
 pattern Leq :: Maybe metadata -> AstNode metadata -> AstNode metadata -> AstNode metadata
-pattern Leq metadata lhs rhs = BinOp "<=" metadata lhs rhs
+pattern Leq metadata lhs rhs = InfixBinOp metadata "<=" lhs rhs
 
 pattern Gt :: Maybe metadata -> AstNode metadata -> AstNode metadata -> AstNode metadata
-pattern Gt metadata lhs rhs = BinOp ">" metadata lhs rhs
+pattern Gt metadata lhs rhs = InfixBinOp metadata ">" lhs rhs
 
 pattern Geq :: Maybe metadata -> AstNode metadata -> AstNode metadata -> AstNode metadata
-pattern Geq metadata lhs rhs = BinOp ">=" metadata lhs rhs
+pattern Geq metadata lhs rhs = InfixBinOp metadata ">=" lhs rhs
 
 exampleProgram :: AstNode metadata
 exampleProgram =
@@ -122,7 +140,7 @@ exampleProgram =
         (And Nothing [Text Nothing "q", Text Nothing "r"]),
       Rule
         Nothing
-        (Parenthesised Nothing [Variable Nothing "x", Text Nothing "is between 0 and 10 or is 100"])
+        (Parens Nothing [Variable Nothing "x", Text Nothing "is between 0 and 10 or is 100"])
         ( Or
             Nothing
             [ And
@@ -133,8 +151,8 @@ exampleProgram =
               Is Nothing (Variable Nothing "x") (Number Nothing 100)
             ]
         ),
-        Fact Nothing $ Parenthesised Nothing [Date Nothing 2023 1 10, Text Nothing "is a date"]
+        Fact Nothing $ Parens Nothing [Date Nothing 2023 1 10, Text Nothing "is a date"]
     ]
 
 --- >>> astToEdn exampleProgram 
--- "[(DECIDE p IF (q AND r)) (DECIDE (var/x is between 0 and 10 or is 100) IF (((0.0 <= var/x) AND (var/x <= 10.0)) OR (var/x IS 100.0))) (DECIDE ((2023 - 1 - 10) is a date) IF ())]"
+-- "[(DECIDE p IF (q AND r)) (DECIDE (var/x is between 0 and 10 or is 100) IF (((0.0 <= var/x) AND (var/x <= 10.0)) OR (var/x IS 100.0))) (DECIDE ((2023 - 1 - 10) is a date))]"

6ddc39e2f079bd025bc15d6392df0e72e4edf4fc
Joe Watt <joewatt95@u.nus.edu>
2024-03-19 22:04:15 +0800

    use tailSafe

--- lib/haskell/natural4/src/LS/XPile/Edn/Utils.hs
+++ lib/haskell/natural4/src/LS/XPile/Edn/Utils.hs
@@ -8,10 +8,11 @@ module LS.XPile.Edn.Utils
 where
 
 import Data.Foldable qualified as Fold
+import Safe (tailSafe)
 
-listToPairs :: Foldable t => t a -> [(a, a)]
+listToPairs :: (Foldable t) => t a -> [(a, a)]
 listToPairs (Fold.toList -> xs) =
-  [(x, y) | (x, y, index) <- zip3 xs (tail xs) [0..], even index]
+  [(x, y) | (x, y, index) <- zip3 xs (tailSafe xs) [0 ..], even index]
 
-pairsToList :: Foldable t => t (a, a) -> [a]
+pairsToList :: (Foldable t) => t (a, a) -> [a]
 pairsToList = foldMap \(x, y) -> [x, y]

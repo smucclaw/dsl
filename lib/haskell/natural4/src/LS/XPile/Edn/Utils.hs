d188801518d883729ba5e6a4f10c523805939120
joewatt95 <joewatt95@u.nus.edu>
2024-03-20 15:59:51 +0800

    format

--- lib/haskell/natural4/src/LS/XPile/Edn/Utils.hs
+++ lib/haskell/natural4/src/LS/XPile/Edn/Utils.hs
@@ -12,7 +12,7 @@ import Safe (tailSafe)
 
 listToPairs :: (Foldable t) => t a -> [(a, a)]
 listToPairs (Fold.toList -> xs) =
-  [(x, y) | (x, y, index) <- zip3 xs (tailSafe xs) [0 ..], even index]
+  [(x, y) | (x, y, index) <- zip3 xs (tailSafe xs) [0..], even index]
 
 pairsToList :: (Foldable t) => t (a, a) -> [a]
 pairsToList = foldMap \(x, y) -> [x, y]

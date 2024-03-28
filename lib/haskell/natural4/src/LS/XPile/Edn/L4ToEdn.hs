f6bcb47f9363342e9579e561fbb89f50d80d88bd
joewatt95 <joewatt95@u.nus.edu>
2024-03-28 14:43:42 +0800

    add l4rulesToEdnText

--- lib/haskell/natural4/src/LS/XPile/Edn/L4ToEdn.hs
+++ lib/haskell/natural4/src/LS/XPile/Edn/L4ToEdn.hs
@@ -1,10 +1,19 @@
-module LS.XPile.Edn.L4ToEdn (l4rulesToEdn) where
+module LS.XPile.Edn.L4ToEdn
+  ( l4rulesToEdn,
+    l4rulesToEdnText,
+  )
+where
 
 import Control.Arrow ((>>>))
+import Data.Text qualified as T
 import LS.Rule (Rule)
 import LS.XPile.Edn.AstToEdn (astNodeToEdn)
-import LS.XPile.Edn.AstToEdn.CPSTranspileM (TranspileResult)
+import LS.XPile.Edn.AstToEdn.CPSTranspileM (TranspileResult, ednText)
 import LS.XPile.Edn.L4ToAst (l4rulesToProgram)
+import Optics.Getter qualified as Optics
 
 l4rulesToEdn :: [Rule] -> TranspileResult metadata
 l4rulesToEdn = l4rulesToProgram >>> astNodeToEdn
+
+l4rulesToEdnText :: [Rule] -> T.Text
+l4rulesToEdnText = l4rulesToEdn >>> Optics.view ednText
